8ddcfcbb91b6f37e858bb8713fed3aaa065a3533
gindik <gindik@ca.ibm.com>
2017-08-30 16:28:40 -0400

    Guides implementation

---
+++ src/main/content/guides/guide-rest-systemprops/finish/src/main/java/io/openliberty/guides/rest/SystemApplication.java
@@ -0,0 +1,9 @@
+package io.openliberty.guides.rest;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("System")
+public class SystemApplication extends Application {
+
+}
--- src/main/content/js/guides.js
+++ src/main/content/js/guides.js
@@ -1,26 +1,60 @@
 $(document).ready(function() {
 
-    function filter_guides(key) {
+    let title_key = 1;
+    let description_key = 2;
+    let tags_key = 4;
+
+
+    function filter_guides(key, search_value) {
         $('.guide_item').each(function(index, element) {
             
             let guide_item = $(element);
-            let hash = guide_item.data('hash');
+            let title = guide_item.data('title');
+            let description = guide_item.data('description');
+            let tags = guide_item.data('tags');
 
-            if(hash.indexOf(key) != -1) {
+            if (((key & title_key) && title.indexOf(search_value) != -1)
+             || ((key & description_key) && description.indexOf(search_value) != -1)
+             || ((key & tags_key) && tags.indexOf(search_value) != -1)) {
                 guide_item.parent().removeClass('hidden');
             } else {
                 guide_item.parent().addClass('hidden');
             }
+
         });
     }
 
     $('#guide_search_input').keyup(function(event) {
-        let value = event.currentTarget.value.toLowerCase();
-        if(value.length == 0) {
+        let input_value = event.currentTarget.value.toLowerCase();
+        if(input_value.length == 0) {
             $('.guide_column').removeClass('hidden');
         } else {
-            filter_guides(value);
+            if(input_value.startsWith('tag:')) {
+                let search_value = input_value.substring(4).trim();
+                filter_guides(tags_key, search_value);
+            } else {
+                filter_guides(title_key | description_key | tags_key, input_value);
+            }
         }
     });
 
+    let query_string = location.search;
+    if(query_string.length > 0) {
+        let parameters = decodeURI(query_string.substring(1)).split('&');
+        let search_value = false;
+        let search_key = false;
+        for(let i = 0; i < parameters.length; i++) {
+
+            if(parameters[i].startsWith('search=')) {
+                search_value = parameters[i].substring(7);
+            } else if (parameters[i].startsWith('key=')) {
+                search_key = parameters[i].substring(4);
+            }
+        }
+        if(search_value) {
+            let input_text = search_key? search_key + ':' + search_value : search_value;
+            $('#guide_search_input').val(input_text).keyup();
+        }
+    }
+    
 });
