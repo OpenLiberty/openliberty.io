b91a7650a5132cdba4cb668c76a0e71e85ad80f2
Gabriel Indik <gindik@ca.ibm.com>
2017-08-31 15:43:56 -0400

    Script for builds

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install curl
+curl -sSL https://get.rvm.io | bash -s stable
+source /home/pipeline/.rvm/scripts/rvm
+rvm requirements
+rvm install 2.4.1
+rvm use 2.4.1 --default
+sudo apt-get -y install nodejs
+gem install jekyll bundler jekyll-feed jekyll-asciidoc coderay
+export JEKYLL_ENV=production
+jekyll build --source src/main/content --destination src/main/webapp
+mvn -B package
