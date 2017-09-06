5b8e3d04ef2157817517221204a654c8e664352f
Alasdair Nottingham <notatibm@gmail.com>
2017-09-06 15:08:16 -0400

    Add in the rest guides and common files for build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,8 @@ rvm use 2.4.1 --default
 sudo apt-get -y install nodejs
 gem install jekyll bundler jekyll-feed jekyll-asciidoc coderay
 export JEKYLL_ENV=production
+git clone "https://gindik:$PAT@github.com/OpenLiberty/guides-common.git" src/main/content/guides/guides-common
+git clone "https://gindik:$PAT@github.com/OpenLiberty/guide-rest-intro.git" src/main/content/guides/guide_rest_intro
 git clone "https://gindik:$PAT@github.com/OpenLiberty/guide-maven.git" src/main/content/guides/guide_maven
 jekyll build --source src/main/content --destination src/main/webapp
 mvn -B package
--- src/main/content/guides/guide-rest-systemprops/finish/src/main/java/io/openliberty/guides/rest/SystemApplication.java
+++
@@ -1,9 +0,0 @@
-package io.openliberty.guides.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("System")
-public class SystemApplication extends Application {
-
-}
