801b520dbf790c4f1e5b597b4b3b46d546669b46
Alasdair Nottingham <notatibm@gmail.com>
2017-09-08 22:09:56 -0400

    Only use required features for Liberty

--- scripts/deploy_function.sh
+++ scripts/deploy_function.sh
@@ -2,7 +2,7 @@
 function pushApp {
   cf push "${CF_APP}" --no-start
   cf set-env "${CF_APP}" PAT "${PAT}"
-  cf set-env myapp JBP_CONFIG_LIBERTY "app_archive: {features: [jaxrs-2.0]}"
+  cf set-env myapp JBP_CONFIG_LIBERTY "app_archive: {features: [$FEATURES]}"
   if [[ -z $CURRENT_STATE ]]
   then
     cf bind-service "${CF_APP}" "${AVAILABILITY_SERVICE}"
