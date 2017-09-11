cc0d6eadbf6a95088951b721d642543d7568ec4e
gindik <gindik@ca.ibm.com>
2017-09-11 15:24:44 -0400

    Address build issue

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
-sudo apt-get install curl
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/pipeline/.rvm/scripts/rvm
 rvm requirements
