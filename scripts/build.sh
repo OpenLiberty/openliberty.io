b8d48c418e647551a748fb4eee2cf95d882f9dc2
gindik <gindik@ca.ibm.com>
2017-09-22 15:46:16 -0400

    Remove octopress dependency

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ rvm requirements
 rvm install 2.4.1
 rvm use 2.4.1 --default
 sudo apt-get -y install nodejs
-gem install jekyll bundler jekyll-feed jekyll-asciidoc coderay octopress-minify-html
+gem install jekyll bundler jekyll-feed jekyll-asciidoc coderay
 git clone "https://github.com/OpenLiberty/guides-common.git" src/main/content/guides/guides-common
 git clone "https://github.com/OpenLiberty/guide-rest-intro.git" src/main/content/guides/guide_rest_intro
 git clone "https://github.com/OpenLiberty/guide-maven-intro.git" src/main/content/guides/guide_maven_intro
