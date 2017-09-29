bffdb9dfacb8618679fb12379b4dd8abaeaa15cd
Alasdair Nottingham <alasdair.nottingham@gmail.com>
2017-09-29 18:27:41 -0400

    Update for 17.0.0.3 release

--- src/main/content/latestVersion.js
+++ src/main/content/latestVersion.js
@@ -1,5 +1,5 @@
 var latestReleasedVersion = { 
-  version: "17.0.0.3-RC",
+  version: "17.0.0.3",
   productName : "Open Liberty Runtime",
   availableFrom : "https://openliberty.io/downloads?welcome"
   };
