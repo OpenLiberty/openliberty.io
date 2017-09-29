959f9584b5151d329c7c613b24b3966ae8a2cd53
gindik <gindik@ca.ibm.com>
2017-09-29 15:25:45 -0400

    Add copyright headers

--- src/main/content/latestVersion.js
+++ src/main/content/latestVersion.js
@@ -1,5 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
 var latestReleasedVersion = { 
-  version: "17.0.0.3-RC",
-  productName : "Open Liberty Runtime",
-  availableFrom : "https://openliberty.io/downloads?welcome"
-  };
+    version: '17.0.0.3-RC',
+    productName : 'Open Liberty Runtime',
+    availableFrom : 'https://openliberty.io/downloads?welcome'
+};
