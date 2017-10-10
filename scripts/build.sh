5ff3fc2c7001b8e96d4d1ddbbf72ece3b3ad3533
Kin Ueng <kueng@us.ibm.com>
2017-10-10 09:32:50 -0500

    Split the build.sh to be a script for setting up the VM and a script to build jekyll

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,24 +7,6 @@ rvm requirements
 rvm install 2.4.1
 rvm use 2.4.1 --default
 sudo apt-get -y install nodejs
-gem install jekyll bundler jekyll-feed jekyll-asciidoc coderay jekyll-assets uglifier octopress-minify-html
-git clone "https://github.com/OpenLiberty/guides-common.git" src/main/content/guides/guides-common
-git clone "https://github.com/OpenLiberty/guide-rest-intro.git" src/main/content/guides/guide_rest_intro
-git clone "https://github.com/OpenLiberty/guide-maven-intro.git" src/main/content/guides/guide_maven_intro
-git clone "https://github.com/OpenLiberty/guide-microprofile-intro.git" src/main/content/guides/guide_microprofile_intro
-git clone "https://github.com/OpenLiberty/guide-rest-hateoas.git" src/main/content/guides/guide_rest_hateoas
-git clone "https://github.com/OpenLiberty/guide-rest-client-java" src/main/content/guides/guide_rest_client_java
-git clone "https://github.com/OpenLiberty/guide-maven-multimodules" src/main/content/guides/guide_maven_multimodules
-git clone "https://github.com/OpenLiberty/guide-cors" src/main/content/guides/guide_cors
 
-# Clone the circuit breaker interactive guide.
-git clone "https://github.com/OpenLiberty/iguides-common" --branch master --single-branch src/main/content/guides/iguides-common
-git clone "https://github.com/OpenLiberty/iguide-circuit-breaker" --branch master --single-branch src/main/content/guides/iguide-circuit-breaker
-# Move any js/css files from guides to the _assets folder for jekyll-assets minification.
-find src/main/content/guides/iguide* -d -name js -exec cp -R '{}' src/main/content/_assets \;
-find src/main/content/guides/iguide* -d -name css -exec cp -R '{}' src/main/content/_assets \;
-
-mkdir target
-mkdir target/jekyll-webapp
-jekyll build --source src/main/content --destination target/jekyll-webapp
-mvn -B package
+SCRIPT_DIR=$(dirname $0)
+source $SCRIPT_DIR/build_jekyll.sh
