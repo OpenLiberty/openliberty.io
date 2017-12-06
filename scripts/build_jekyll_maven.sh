2877cc1f474280c7050183e9474cd658ed4c7e9e
Kin Ueng <kueng@us.ibm.com>
2017-12-05 22:42:24 -0600

    Add interactive microprofile config guide and misc. - Include more build logging - Change GitHub urls to be the same

--- scripts/build_jekyll_maven.sh
+++ scripts/build_jekyll_maven.sh
@@ -12,14 +12,14 @@ git clone "https://github.com/OpenLiberty/guide-rest-intro.git" src/main/content
 git clone "https://github.com/OpenLiberty/guide-maven-intro.git" src/main/content/guides/guide_maven_intro
 git clone "https://github.com/OpenLiberty/guide-microprofile-intro.git" src/main/content/guides/guide_microprofile_intro
 git clone "https://github.com/OpenLiberty/guide-rest-hateoas.git" src/main/content/guides/guide_rest_hateoas
-git clone "https://github.com/OpenLiberty/guide-rest-client-java" src/main/content/guides/guide_rest_client_java
-git clone "https://github.com/OpenLiberty/guide-maven-multimodules" src/main/content/guides/guide_maven_multimodules
-git clone "https://github.com/OpenLiberty/guide-cors" src/main/content/guides/guide_cors
-git clone "https://github.com/OpenLiberty/guide-rest-client-angularjs" src/main/content/guides/guide-rest-client-angularjs
+git clone "https://github.com/OpenLiberty/guide-rest-client-java.git" src/main/content/guides/guide_rest_client_java
+git clone "https://github.com/OpenLiberty/guide-maven-multimodules.git" src/main/content/guides/guide_maven_multimodules
+git clone "https://github.com/OpenLiberty/guide-cors.git" src/main/content/guides/guide_cors
+git clone "https://github.com/OpenLiberty/guide-rest-client-angularjs.git" src/main/content/guides/guide-rest-client-angularjs
 
 # List of interactive guides
-git clone "https://github.com/OpenLiberty/iguides-common" --branch master --single-branch src/main/content/guides/iguides-common
-git clone "https://github.com/OpenLiberty/iguide-circuit-breaker" --branch master --single-branch src/main/content/guides/iguide-circuit-breaker
+git clone "https://github.com/OpenLiberty/iguides-common.git" --branch master --single-branch src/main/content/guides/iguides-common
+git clone "https://github.com/OpenLiberty/iguide-circuit-breaker.git" --branch master --single-branch src/main/content/guides/iguide-circuit-breaker
 
 # Development environment only actions
 if [ "$JEKYLL_ENV" != "production" ]; then
@@ -29,6 +29,7 @@ if [ "$JEKYLL_ENV" != "production" ]; then
     
     echo "Clone guides that are only for test site..."
     git clone "https://github.com/OpenLiberty/guide-microprofile-config.git" src/main/content/guides/guide_microprofile_config
+    git clone "https://github.com/OpenLiberty/iguide-microprofile-config.git" src/main/content/guides/iguide-microprofile-config
 fi
 
 # Move any js/css files from guides to the _assets folder for jekyll-assets minification.
@@ -37,6 +38,8 @@ find src/main/content/guides/iguide* -d -name js -exec cp -R '{}' src/main/conte
 find src/main/content/guides/iguide* -d -name css -exec cp -R '{}' src/main/content/_assets \;
 
 # Jekyll build
+echo "Building with jekyll..."
+echo `jekyll -version`
 mkdir target
 mkdir target/jekyll-webapp
 jekyll build --source src/main/content --destination target/jekyll-webapp
