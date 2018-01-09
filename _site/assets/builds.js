3cb5a9e22b9c573dab46b04a46bc7542fbbda3fa
Kin Ueng <kueng@us.ibm.com>
2018-01-09 15:47:12 -0600

    Working collapsable left side

---
+++ _site/assets/builds.js
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+
+var builds = [];
+
+var runtime_releases = [];
+var runtime_development_builds = [];
+var developer_tools_releases = [];
+var developer_tools_development_builds = [];
+
+var builds_url = '/api/builds/data';
+
+function render_builds(builds, parent) {
+
+    parent.empty();
+
+    var analytics_class_name = 'link_' + parent.parent().data('builds-id');
+
+    builds.forEach(function(build) {
+
+        var row = $('<tr></tr>');        
+        
+        if(parent.hasClass('release_table_body')) {
+            var version_column = $('<td><span class="table_date">' + build.version + '</span></td>');
+            row.append(version_column);
+        } else {
+            var date = new Date(build.date);
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var day = date.getDate();
+            var hour = date.getHours();
+            var minute = date.getMinutes();
+            var date_column = $('<td><span class="table_date">' + year + '-' + add_lead_zero(month) + '-' + add_lead_zero(day) + ', ' + add_lead_zero(hour) + ':' + add_lead_zero(minute) + '</span></td>');
+            row.append(date_column);
+            
+            var tests_column = $('<td><a href="' +  build.tests_log +'" target="new" class="'
+             + analytics_class_name + ' skip_outbound_link_analytics tests_passed_link">' + build.test_passed + ' / ' + build.total_tests + '</a></td>');
+            row.append(tests_column);
+            
+            var log_column = $('<td><a href="' + build.build_log + '" target="new" class="'
+            + analytics_class_name + ' skip_outbound_link_analytics view_logs_link">View logs</a></td>');            
+            row.append(log_column);
+        }
+
+        var zip_column = $('<td><a href="' + build.driver_location + '" class="' + analytics_class_name + ' skip_outbound_link_analytics build_download_button">Download (.zip)</a></td>');
+        
+        row.append(zip_column);
+
+        parent.append(row);
+
+    });
+}
+
+
+
+function add_lead_zero(number) {
+    if(number < 10) {
+        return '0' + number;
+    } else {
+        return number;
+    }
+}
+
+
+
+function sort_builds(builds, key, descending) {
+    builds.sort(function(a, b) {
+        if(descending) {
+            return a[key] < b[key]? 1 : -1;
+        } else {
+            return a[key] > b[key]? 1 : -1;
+        }
+    });
+}
+
+
+
+$(document).ready(function() {
+
+    $('.builds_expand_link').click(function(event) {
+        event.preventDefault();
+        var table_container = $('#' + event.currentTarget.getAttribute('data-table-container-id'));
+
+        var rows = $('tbody tr', table_container).length;
+        var delay = 400 + rows * 25;
+
+        if(table_container.is(':visible')) {
+            table_container.animate({opacity: 0}, 400, function() {
+                table_container.slideUp(delay, function() {
+                    $('.collapse_link_text', event.currentTarget).text('expand');
+
+                });
+            });
+        } else {
+            table_container.slideDown(delay, function() {
+                table_container.animate({opacity: 1}, 400);
+                $('.collapse_link_text', event.currentTarget).text('collapse');
+            });
+        }
+       
+    });
+
+
+
+    $('.build_table thead tr th a').click(function(event) {
+        event.preventDefault();
+
+        var table = $(event.currentTarget).closest('table');
+
+        var builds_id = table.data('builds-id');
+        var key = event.currentTarget.getAttribute('data-key');
+        var descending = !(event.currentTarget.getAttribute('data-descending') == 'true');
+
+        event.currentTarget.setAttribute('data-descending', descending);
+        
+        sort_builds(builds[builds_id], key, descending);
+        render_builds(builds[builds_id], $('tbody', table));
+
+        $('th .table_header_arrow', table).removeClass('table_header_arrow_down table_header_arrow_up');
+        $('.table_header_arrow', event.currentTarget).addClass(descending? 'table_header_arrow_down' : 'table_header_arrow_up');
+
+    });
+
+
+
+    $.ajax({
+        url: builds_url
+    }).done(function(data) {
+
+        $('#runtime_download_button_version').text(data.latest_releases.runtime.version);
+        $('#eclipse_developer_tools_download_link_version_text').text(data.latest_releases.tools.version);
+
+        $('#runtime_download_link').attr('href', data.latest_releases.runtime.driver_location);
+        $('#eclipse_developer_tools_download_link').attr('href', data.latest_releases.tools.driver_location);
+
+        runtime_releases = formatBuilds(data.builds.runtime_releases);
+        developer_tools_releases = formatBuilds(data.builds.tools_releases);
+        runtime_development_builds = formatBuilds(data.builds.runtime_nightly_builds);
+        developer_tools_development_builds = formatBuilds(data.builds.tools_nightly_builds);
+
+        function formatBuilds(builds_from_response) {
+            for(var i = 0; i < builds_from_response.length; i++) {
+                var date_string = builds_from_response[i].date_time;
+                var date = new Date(date_string.substr(0, 4), date_string.substr(5, 2) - 1, date_string.substr(8, 2), date_string.substr(11, 2), date_string.substr(13, 2));
+                builds_from_response[i].date = date.getTime();
+            }
+            return builds_from_response;
+        }
+
+        builds['runtime_releases'] = runtime_releases;
+        builds['runtime_development_builds'] = runtime_development_builds;
+        builds['developer_tools_releases'] = developer_tools_releases;
+        builds['developer_tools_development_builds'] = developer_tools_development_builds;
+
+        sort_builds(runtime_releases, 'version', true);
+        render_builds(runtime_releases, $('table[data-builds-id="runtime_releases"] tbody'));
+
+        sort_builds(runtime_development_builds, 'date', true);
+        render_builds(runtime_development_builds, $('table[data-builds-id="runtime_development_builds"] tbody'));
+
+        sort_builds(developer_tools_releases, 'version', true);
+        render_builds(developer_tools_releases, $('table[data-builds-id="developer_tools_releases"] tbody'));
+
+        sort_builds(developer_tools_development_builds, 'date', true);
+        render_builds(developer_tools_development_builds, $('table[data-builds-id="developer_tools_development_builds"] tbody'));
+
+    });
+    
+});
---
+++ _site/assets/issues.js
@@ -0,0 +1,188 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+
+var issues = [];
+var focus_issue_index = 0;
+var scroll_in_progress = false;
+var issues_url = '/api/github/issues';
+
+$('#issues_up_arrow').click(function(event) {
+    event.preventDefault();
+    scroll(true);
+});
+
+
+
+$('#issues_down_arrow').click(function(event) {
+    event.preventDefault();
+    scroll();
+});
+
+
+
+$('#issues_content').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', '.issue_0, .issue_6', function(event) {
+    $(event.currentTarget).remove();
+});
+
+
+$('#issues_content').on('mousewheel wheel DOMMouseScroll', function(event) {
+    if(issues.length > 0) {
+        event.preventDefault();
+        if(event.originalEvent.deltaY < 0 || event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+            scroll(true);
+        }
+        else {
+            scroll();
+        }
+    }
+});
+
+
+
+function scroll(up) {
+    if(!scroll_in_progress) {
+        scroll_in_progress = true;
+        if(up) {
+            if(issues.length > 4 || (issues.length == 4 && focus_issue_index == 3)) {
+                var offset = (issues.length + focus_issue_index) - 3;
+                if(offset + 1 > issues.length) {
+                    offset = offset - issues.length;
+                }
+                var new_issue_element = create_issue_element(offset, 0);
+                $('#issues_content').prepend(new_issue_element);
+            }
+            if(issues.length > 4 || focus_issue_index > 0) {
+                $('#issues_content .issue_5').toggleClass('issue_5 issue_6');
+                $('#issues_content .issue_4').toggleClass('issue_4 issue_5');
+                $('#issues_content .issue_3').toggleClass('issue_3 issue_4');
+                $('#issues_content .issue_2').toggleClass('issue_2 issue_3');
+                $('#issues_content .issue_1').toggleClass('issue_1 issue_2');
+                $('#issues_content .issue_0').toggleClass('issue_0 issue_1');
+                if(focus_issue_index > 0) {
+                    focus_issue_index--;
+                } else {
+                    focus_issue_index = issues.length - 1;
+                }
+            }
+        } else {
+            offset = focus_issue_index + 3;
+            if(issues.length > offset) {
+                new_issue_element = create_issue_element(offset, 6);
+                $('#issues_content').prepend(new_issue_element);
+            } else if(issues.length > 4) {
+                offset = (focus_issue_index - issues.length) + 3;
+                new_issue_element = create_issue_element(offset, 6);
+                $('#issues_content').prepend(new_issue_element);
+            }
+            if(issues.length > 4 || focus_issue_index + 1 < issues.length) {
+                $('#issues_content .issue_1').toggleClass('issue_1 issue_0');
+                $('#issues_content .issue_2').toggleClass('issue_2 issue_1');
+                $('#issues_content .issue_3').toggleClass('issue_3 issue_2');
+                $('#issues_content .issue_4').toggleClass('issue_4 issue_3');
+                $('#issues_content .issue_5').toggleClass('issue_5 issue_4');
+                $('#issues_content .issue_6').toggleClass('issue_6 issue_5');
+                if(focus_issue_index + 1 < issues.length) {
+                    focus_issue_index++;
+                } else {
+                    focus_issue_index = 0;
+                }
+            }
+        }
+        update_tab_index();
+        window.setTimeout(function() {
+            scroll_in_progress = false;
+        }, 400);
+    }
+}
+
+
+
+function update_tab_index() {
+    $('#issues_content .issue').attr('tabindex', '-1');
+    $('#issues_content .issue_3').removeAttr('tabindex');
+}
+
+
+
+function create_issue_element(index, ui_position) {
+    var issue = issues[index];
+    var issue_element = $('<a href="' + issue.html_url + '" target="new" class="issue issue_' + ui_position + ' clearfix center-block"</a>');
+    var bar_element = $('<div class="issue_green_bar"></div>');
+    var title_element = $('<h3 class="truncate">' + issue.title + '</h3>');
+    var date_element = $('<p class="pull-left">' + new Date(issue.updated_at).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'}) + '</p>');
+    var index_element = $('<p class="pull-right">' + (index + 1) + ' / ' + issues.length + '</p>');
+    var shadow_element = $('<div class="shadow"></div>');
+    issue_element.append(bar_element);
+    issue_element.append(title_element);
+    issue_element.append(date_element);
+    issue_element.append(index_element);
+    issue_element.append(shadow_element);
+    return issue_element;
+}
+
+
+
+function retrieve_github_issues() {
+    var deferred = new $.Deferred();
+    $.ajax({
+        url: issues_url
+    }).done(function(data) {
+        issues = data.filter(function(value){return !value.hasOwnProperty('pull_request')});
+        deferred.resolve();
+    }).fail(function() {
+        deferred.reject();
+    });
+    return deferred;
+}
+
+
+
+function initialize_issues() {
+    show_issues_message('Loading issues...');
+    retrieve_github_issues().done(function() {
+        
+        show_issues_message();
+
+        for(var i = 0; i < issues.length && i < 3; i++) {
+            var issue_element = create_issue_element(i, i + 3);
+            $('#issues_content').append(issue_element);
+        }
+
+        if(issues.length > 4) {
+            var issue_element_2 = create_issue_element(issues.length - 1, 2);
+            $('#issues_content').prepend(issue_element_2);
+            var issue_element_1 = create_issue_element(issues.length - 2, 1);
+            $('#issues_content').prepend(issue_element_1);
+        }
+
+        update_tab_index();
+
+    }).fail(function() {
+        show_issues_message('Could not load issues.');
+    });
+}
+
+
+
+function show_issues_message(message) {
+    var message_container = $('#issues_message');
+    message_container.text(message);
+    if(!message) {
+        message_container.addClass('hidden');
+    }
+}
+
+
+
+$(document).ready(function() {
+    initialize_issues();
+});
