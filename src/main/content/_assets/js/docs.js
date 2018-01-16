f6572378c22aa36dd43111f9ddd358c0370d82e4
Kin Ueng <kueng@us.ibm.com>
2018-01-16 15:35:38 -0600

    Working collapsable left side

--- src/main/content/_assets/js/docs.js
+++ src/main/content/_assets/js/docs.js
@@ -9,14 +9,83 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 
+// Make sure the footer and header of the documentation page is always in the
+// browser viewport.
 function resizeJavaDocWindow() {
     var topSection = $('#background_container').height();
-    var bottomSection = $('#footer_container').height();
+    // var bottomSection = $('#footer_container').height();
+    var bottomSection = 0;
 
     var middleSectionHeight = $(window).height() - (topSection + bottomSection);
     $('#javadoc_container').height(middleSectionHeight);
 
     $("body").css("overflow" , "hidden");
+}
+
+function addExpandAndCollapseToggleButtons() {
+    var javadoc_iframes = $('#javadoc_container').contents().find("iframe");
+    $( javadoc_iframes ).each(function() {
+        // Look for the two left side iframes
+        var isTopLeftPackageIFrame = $(this).attr("name") === "packageListFrame";
+        var isBottomLeftPackageIFrame = $(this).attr("name") === "packageFrame";
+
+        if(isTopLeftPackageIFrame) {
+            var list = $(this).contents().find('ul[title="Packages"]');
+            var header = $(this).contents().find("h2[title='Packages']");
+            var toggleButton = $('<input id="top_left_toggle" type="checkbox" checked>');
+            toggleButton.change(function() {
+                // this will contain a reference to the checkbox   
+                if (this.checked) {
+                    list.show();
+
+                } else {
+                    list.hide();
+                }
+            });
+            header.append(toggleButton);
+            header.addClass("leftFrameHeaderStyling");
+        }
+        if(isBottomLeftPackageIFrame) {
+            var list2 = $(this).contents().find('main.indexContainer');
+
+            // I did not know how to select for text that contained whitespace.
+            // example: "All Classes"
+            var header2 = $(this).contents().find("h1:contains('Classes')");
+
+            // .text() returns encoded spaces, convert back to normal spaces 
+            // for string comparison.
+            var header2_text = header2.text().replace('/\s/g',' ').trim();
+            if(header2_text === "All Classes") {
+                var toggleButton2 = $('<input id="top_left_toggle" type="checkbox" checked>');
+                toggleButton2.change(function() {
+                    // this will contain a reference to the checkbox   
+                    if (this.checked) {
+                        list2.show();
+    
+                    } else {
+                        list2.hide();
+                    }
+                });
+                header2.append(toggleButton2);
+                header2.addClass("leftFrameHeaderStyling");
+            }
+        }
+    });
+}
+
+// We want to apply our own custom styling to the default javadocs
+function addCustomCssToJavaDocs() {
+    // var customCss = $("head > link[href*='javadocs.css']"),
+    //     rawHtml = customCss.prop('outerHTML'),
+    var javadoc_iframes = $('#javadoc_container').contents().find("iframe");
+
+    $( javadoc_iframes ).each(function() {
+        console.log(this);
+        var head = $(this).contents().find("head");
+        head.append('<link rel="stylesheet" type="text/css" href="javadocs.css" title="Style">');
+    });
+    
+    //iframe_head.append('<link rel="stylesheet" type="text/css" href="javadocs.css" title="Custom Style">');
 }
 
 $(document).ready(function() {
@@ -27,12 +96,10 @@ $(document).ready(function() {
         resizeJavaDocWindow();
     });
 
-    // javadocs/liberty-javaee7-javadoc/index.html
-    // javadocs/microprofile-1.2-javadoc/index.html
-    $('#liberty').on('click', function(){
-        $('#javadoc_container').prop('src', "javadocs/liberty-javaee7-javadoc/index.html");
-    });
-    $('#mp').on('click', function(){
-        $('#javadoc_container').prop('src', "javadocs/microprofile-1.2-javadoc/index.html");
-    });
+    $('#javadoc_container').load(function() {
+        // After the javadocs are loaded, always apply our custom javadoc stylesheet
+        // addCustomCssToJavaDocs();
+
+        addExpandAndCollapseToggleButtons();
+    })
 });
