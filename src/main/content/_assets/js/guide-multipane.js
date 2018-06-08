f3ed339efa861c3fd1f7e8479ef5e62903ca2340
steven1046 <stevenzvonek1992@gmail.com>
2018-06-08 14:42:16 -0500

    Implement arrow for code action block

--- src/main/content/_assets/js/guide-multipane.js
+++ src/main/content/_assets/js/guide-multipane.js
@@ -253,34 +253,34 @@ $(document).ready(function() {
         $("#guide_column").addClass('expanded');
     });
 
-    $('#guide_content pre:not(.command pre)').hover(function(event) {
+    // $('#guide_content pre:not(.command pre)').hover(function(event) {
 
-        offset = $('#guide_column').position();
-        target = event.currentTarget;
-        var current_target_object = $(event.currentTarget);
-        target_position = current_target_object.position();
-        target_width = current_target_object.outerWidth();
-        target_height = current_target_object.outerHeight();
+    //     offset = $('#guide_column').position();
+    //     target = event.currentTarget;
+    //     var current_target_object = $(event.currentTarget);
+    //     target_position = current_target_object.position();
+    //     target_width = current_target_object.outerWidth();
+    //     target_height = current_target_object.outerHeight();
 
-        $('#copy_to_clipboard').css({
-            top: target_position.top + 8,
-            right: parseInt($('#guide_column').css('padding-right')) + 55
-        });
-        $('#copy_to_clipboard').stop().fadeIn();
+    //     $('#copy_to_clipboard').css({
+    //         top: target_position.top + 8,
+    //         right: parseInt($('#guide_column').css('padding-right')) + 55
+    //     });
+    //     $('#copy_to_clipboard').stop().fadeIn();
 
-    }, function(event) {
+    // }, function(event) {
 
-        var x = event.clientX - offset.left;
-        var y = event.clientY - offset.top + $(window).scrollTop();
-        if(!(x > target_position.left
-        && x < target_position.left + target_width
-        && y > target_position.top
-        && y < target_position.top + target_height)) {
-            $('#copy_to_clipboard').stop().fadeOut();
-            $('#copied_to_clipboard_confirmation').stop().fadeOut();
-        }  
+    //     var x = event.clientX - offset.left;
+    //     var y = event.clientY - offset.top + $(window).scrollTop();
+    //     if(!(x > target_position.left
+    //     && x < target_position.left + target_width
+    //     && y > target_position.top
+    //     && y < target_position.top + target_height)) {
+    //         $('#copy_to_clipboard').stop().fadeOut();
+    //         $('#copied_to_clipboard_confirmation').stop().fadeOut();
+    //     }  
 
-    });
+    // });
 
     $('#copy_to_clipboard').click(function(event) {
         
