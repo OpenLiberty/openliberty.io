ae199ebc5b86041a899ab837ec5b30ceae9378d5
steven1046 <stevenzvonek1992@gmail.com>
2018-06-13 11:43:49 -0500

    Remove copy to clipboard since its deprecated with the new multipane

--- src/main/content/_assets/js/guide-multipane.js
+++ src/main/content/_assets/js/guide-multipane.js
@@ -254,53 +254,6 @@ $(document).ready(function() {
         $("#guide_column").addClass('expanded');
     });
 
-    // $('#guide_content pre:not(.command pre)').hover(function(event) {
-
-    //     offset = $('#guide_column').position();
-    //     target = event.currentTarget;
-    //     var current_target_object = $(event.currentTarget);
-    //     target_position = current_target_object.position();
-    //     target_width = current_target_object.outerWidth();
-    //     target_height = current_target_object.outerHeight();
-
-    //     $('#copy_to_clipboard').css({
-    //         top: target_position.top + 8,
-    //         right: parseInt($('#guide_column').css('padding-right')) + 55
-    //     });
-    //     $('#copy_to_clipboard').stop().fadeIn();
-
-    // }, function(event) {
-
-    //     var x = event.clientX - offset.left;
-    //     var y = event.clientY - offset.top + $(window).scrollTop();
-    //     if(!(x > target_position.left
-    //     && x < target_position.left + target_width
-    //     && y > target_position.top
-    //     && y < target_position.top + target_height)) {
-    //         $('#copy_to_clipboard').stop().fadeOut();
-    //         $('#copied_to_clipboard_confirmation').stop().fadeOut();
-    //     }  
-
-    // });
-
-    $('#copy_to_clipboard').click(function(event) {
-        
-        event.preventDefault();
-        window.getSelection().selectAllChildren(target);
-        if(document.execCommand('copy')) {
-            window.getSelection().removeAllRanges();
-            var current_target_object = $(event.currentTarget);
-            var position = current_target_object.position();
-            $('#copied_to_clipboard_confirmation').css({
-                top: position.top - 25,
-                right: 50
-            }).stop().fadeIn().delay(3500).fadeOut();
-        } else {
-            alert('To copy press CTRL + C');
-        }
-
-    });
-
     // Set the github clone popup top value to the same as the what you'll learn section.
     var whatYoullLearn = $("#what-you-ll-learn");
     if(whatYoullLearn.length > 0){
