b134f47a04a14a80fd4d414efcdedbfe88e9ca70
Michael C. Thompson <mcthomps@us.ibm.com>
2018-08-07 15:04:58 -0500

    Fix NPE in test code because I forgot to initialize the DHEBuildParser with a DHEClient

--- src/test/java/io/openliberty/website/dheclient/DHEBuildsParserTest.java
+++ src/test/java/io/openliberty/website/dheclient/DHEBuildsParserTest.java
@@ -7,12 +7,13 @@ import org.junit.Test;
 import io.openliberty.website.data.BuildData;
 import io.openliberty.website.dheclient.DHEBuildParser;
 import io.openliberty.website.mock.MockDHEClient;
+import io.openliberty.website.mock.NullDHEClient;
 
 public class DHEBuildsParserTest {
 
 	@Test
 	public void constructor() {
-		DHEBuildParser parser = new DHEBuildParser();
+		DHEBuildParser parser = new DHEBuildParser(new NullDHEClient());
 		BuildData data = parser.getBuildData();
 		assertNotNull(data.getLatestReleases());
 		assertNotNull(data.getBuilds());
