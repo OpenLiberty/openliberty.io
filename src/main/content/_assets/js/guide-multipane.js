717f9f6a7e30c65005c3160ead0110afcae4e25a
steven1046 <stevenzvonek1992@gmail.com>
2018-08-22 11:03:49 -0500

    Reenable the copy button for code snippets and commands, excluding code commands

--- src/main/content/_assets/js/guide-multipane.js
+++ src/main/content/_assets/js/guide-multipane.js
@@ -406,6 +406,47 @@ $(document).ready(function() {
             }   
         }            
     }
+
+    $('#guide_content pre:not(.code_command pre):not(.hotspot pre)').hover(function(event) {
+         offset = $('#guide_column').position();	
+        target = event.currentTarget;	
+        var current_target_object = $(event.currentTarget);	
+        target_position = current_target_object.position();	
+        target_width = current_target_object.outerWidth();	
+        target_height = current_target_object.outerHeight();	
+         $('#copy_to_clipboard').css({	
+            top: target_position.top + 8,	
+            right: parseInt($('#guide_column').css('padding-right')) + 55	
+        });	
+        $('#copy_to_clipboard').stop().fadeIn();	
+     }, function(event) {	
+         var x = event.clientX - offset.left;	
+        var y = event.clientY - offset.top + $(window).scrollTop();	
+        if(!(x > target_position.left	
+        && x < target_position.left + target_width	
+        && y > target_position.top	
+        && y < target_position.top + target_height)) {	
+            $('#copy_to_clipboard').stop().fadeOut();	
+            $('#copied_to_clipboard_confirmation').stop().fadeOut();	
+        }  	
+     });	
+
+     $('#copy_to_clipboard').click(function(event) {	        	
+        event.preventDefault();	
+        window.getSelection().selectAllChildren(target);	
+        if(document.execCommand('copy')) {	
+            window.getSelection().removeAllRanges();	
+            var current_target_object = $(event.currentTarget);	
+            var position = current_target_object.position();	
+            $('#copied_to_clipboard_confirmation').css({	
+                top: position.top - 25,	
+                right: 50	
+            }).stop().fadeIn().delay(3500).fadeOut();	
+        } else {	
+            alert('To copy press CTRL + C');	
+        }	
+    });
+
     $(window).on('scroll', function(event) {
         handleGithubPopup(false);
         handleSectionSnapping(event);
