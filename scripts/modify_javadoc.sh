660a9acb0d764f6fac9b997d6b504f21bedded33
Lavena Chan <lavena@us.ibm.com>
2018-09-14 11:18:53 -0500

    update shell script to replace search.js for javadoc

--- scripts/modify_javadoc.sh
+++ scripts/modify_javadoc.sh
@@ -1,6 +1,6 @@
 modifyStylesheet () {
     # Append extra javadoc styling located in the css direcctory to the stylesheet.css located in the javadoc subdirectories
-    cat src/main/content/_assets/css/javadoc_extended_stylesheet.css >> "$1"
+    cat src/main/content/_assets/css/javadoc-extended-stylesheet.css >> "$1"
 
     # insert extra import to the beginning of the file
     sed '1 i\
@@ -10,6 +10,13 @@ modifyStylesheet () {
     mv newstylesheet.css "$1"
 }
 
+modifySearch () {
+    # add url support to search
+    cp src/main/content/_assets/js/javadoc-search.js "$1"
+}
+
 export -f modifyStylesheet
+export -f modifySearch
 
 find src/main/content/javadocs -name stylesheet.css -exec bash -c 'modifyStylesheet {}' \;
+find src/main/content/javadocs -name search.js  -exec bash -c 'modifySearch {}' \;
