2a56ddbfa2562b039eb9d58ec268f7a44394afc0
steven1046 <stevenzvonek1992@gmail.com>
2018-09-17 14:29:07 -0500

    Remove dead code

--- src/main/content/_assets/js/header.js
+++ src/main/content/_assets/js/header.js
@@ -1,15 +1,10 @@
 $(document).ready(function(){
     $('#navbar_responsive_button').on('click', function(){     
         $(this).toggle();
-        $('#mobile_close_button').toggle();   
-        // $(this).hide();
-        // $('#mobile_close_button').show();
-        // $('.navbar-header').toggle();
+        $('#mobile_close_button').toggle();  
     });
 
     $('#mobile_close_button').on('click', function(){
-        // $(this).hide();
-        // $('#navbar_responsive_button').show();
         $('#navbar_responsive_button').click();
     });
 
