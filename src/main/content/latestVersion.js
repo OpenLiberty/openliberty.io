3679ede26f63d6e60500c007fa654ebcb242e7a5
Steven Zvonek <szvonek@us.ibm.com>
2018-09-18 16:23:03 -0500

    Update latestVersion.js

--- src/main/content/latestVersion.js
+++ src/main/content/latestVersion.js
@@ -10,7 +10,7 @@
  *******************************************************************************/
 
 var latestReleasedVersion = {
-    version: '18.0.0.2',
+    version: '18.0.0.3',
     productName : 'Open Liberty Runtime',
     availableFrom : 'https://openliberty.io/downloads?welcome'
 };
