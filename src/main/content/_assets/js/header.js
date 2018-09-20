0baf86726727312c7a23e4839f4cf77b2c88b660
steven1046 <stevenzvonek1992@gmail.com>
2018-09-20 12:14:24 -0500

    Add focusout logic for the dropdown menus; Add focus/hover state to moble links section

--- src/main/content/_assets/js/header.js
+++ src/main/content/_assets/js/header.js
@@ -10,19 +10,32 @@ $(document).ready(function(){
         $('#navbar_responsive_button').trigger('click');
     });
 
-    // Toggle between down caret and up caret
-    $('.dropdown_container').on('click', function(){
-        var img = $(this).find('.mobile_caret img');
-        var src = img[0].src;
+    // Toggle between down caret and up caret.
+    // If focusout is set, then the button was clicked out of so reset to a down caret.
+    var toggleDropdownIcon = function(dropdownMenu, focusout){
+        // Check if the dropdown menu is open or closed
+        var button = dropdownMenu.find('button');
+        var open = button.attr('aria-expanded') == "true";
+
+        var img = dropdownMenu.find('.mobile_caret img');
+        img.remove();
+
         var newSVG = $('<img></img>');
-        if(src.indexOf('upcaret') > -1){
+        if(open || focusout){
             newSVG.attr('src','/img/downcaret.svg');    
             newSVG.attr('alt','Open nav menu');   
         } else{
             newSVG.attr('src','/img/upcaret.svg');
             newSVG.attr('alt','Close nav menu');   
-        }
-        img.remove();
-        $(this).find('.mobile_caret').append(newSVG);
+        }        
+        dropdownMenu.find('.mobile_caret').append(newSVG);
+    };
+
+    $('.dropdown_container').on('focusout', function(){   
+        toggleDropdownIcon($(this), true);
+    });
+
+    $('.dropdown_container').on('click', function(){
+        toggleDropdownIcon($(this));
     });
 });
