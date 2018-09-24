8299857a84039bd94ee3486212eff7076c404e52
Kin Ueng <kueng@us.ibm.com>
2018-09-24 11:21:25 -0500

    Debug: remove StickyRouting

--- src/main/java/io/openliberty/website/StickyRouting.java
+++ src/main/java/io/openliberty/website/StickyRouting.java
@@ -8,32 +8,32 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package io.openliberty.website;
+// package io.openliberty.website;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.Filter;
+// import javax.servlet.FilterConfig;
+// import javax.servlet.ServletException;
+// import javax.servlet.FilterChain;
+// import javax.servlet.ServletRequest;
+// import javax.servlet.ServletResponse;
+// import javax.servlet.http.HttpServletRequest;
 
-public class StickyRouting implements Filter {
-    public void destroy() {
-    }
+// public class StickyRouting implements Filter {
+//     public void destroy() {
+//     }
 
-    public void init(FilterConfig cfg) {
-    }
+//     public void init(FilterConfig cfg) {
+//     }
 
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
-            throws IOException, ServletException {
+//     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+//             throws IOException, ServletException {
 
-        if (req instanceof HttpServletRequest) {
-            ((HttpServletRequest) req).getSession();
-        }
+//         if (req instanceof HttpServletRequest) {
+//             ((HttpServletRequest) req).getSession();
+//         }
 
-        chain.doFilter(req, resp);
-    }
-}
+//         chain.doFilter(req, resp);
+//     }
+// }
