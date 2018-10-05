85b2fcf028a86f24a67aad1930f5f21e42612b0d
steven1046 <stevenzvonek1992@gmail.com>
2018-10-05 12:06:39 -0500

    Slide out nav in reverse direction it comes in

--- src/main/content/_assets/js/sticky-header.js
+++ src/main/content/_assets/js/sticky-header.js
@@ -11,26 +11,31 @@
 
 $(document).ready(function(){
     var navbar = $('.navbar');
-    var navHeight = navbar.outerHeight();
     var lastScrollTop = 0;
 
     $(window).on('scroll', function(){
         // Check if scrolling up to apply the stickyNav class.
         var newScrollTop = $(this).scrollTop();
+        
         if (newScrollTop < lastScrollTop){
-            // Scrolling up
+            // SCROLLING UP
             var offset = window.scrollY;
-            if(offset > navHeight / 2){
-                navbar.addClass('stickyNav');             
+            var floating_nav_point = $('.floating_nav_point').offset().top;
+            if(offset > floating_nav_point){ 
+                navbar.addClass('stickyNav');
             } else{
+                navbar.css('top', '0');
                 navbar.removeClass('stickyNav');
             }
         } else{
-            // Scrolling down
-            // Use a threshold of over 3 pixels so the user doesn't accidentally make the navbar dissapear unless they really scroll down.
-            if((newScrollTop - lastScrollTop) > 3){
-                navbar.removeClass('stickyNav');
-            }            
+            // SCROLLING DOWN
+            // Use a threshold of over 5 pixels so the user doesn't accidentally make the navbar dissapear unless they really scroll down.
+            if((newScrollTop - lastScrollTop) > 5){
+                navbar.stop().animate({top: '-75px'}, function(){
+                    navbar.css('top', '0');
+                    navbar.removeClass('stickyNav');
+                });
+            }
         }
         lastScrollTop = newScrollTop;        
     });
