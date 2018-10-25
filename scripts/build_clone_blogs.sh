5cd389c5aa5ab70890324b59b0d3cea65c7daf6c
Kin Ueng <github@kinueng.com>
2018-10-25 14:51:40 -0500

    New build step to include blog repository - Ignore images from OpenLiberty/blogs - Ignore blog posts from OpenLiberty/blogs

---
+++ scripts/build_clone_blogs.sh
@@ -0,0 +1,21 @@
+pushd src/main/content
+
+# For development purposes, lets always delete previously created folders
+# so that you can run this script to refresh your blog files
+rm -rf _posts _drafts
+rm -rf img/blog
+
+echo "Start cloning blogs repository..."
+
+git clone https://github.com/OpenLiberty/blogs.git --branch master blogs_temp
+
+mv blogs_temp/drafts/ .
+mv drafts/ _drafts
+mv blogs_temp/publish/ .
+mv publish/ _posts
+
+mv blogs_temp/img/blog img
+
+rm -rf blogs_temp
+popd
+echo "Done cloning blogs repository!"🚫
