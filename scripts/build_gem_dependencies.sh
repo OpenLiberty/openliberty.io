f27e41f9e6830bcb5598b2f881cc8ae31f00540e
steven1046 <stevenzvonek1992@gmail.com>
2018-10-29 16:07:53 -0500

    Test out new jekyll seo plugin

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,3 +1,3 @@
 echo "Installing ruby packages..."
-gem install jekyll bundler jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
+gem install jekyll bundler jekyll-feed jekyll-asciidoc jekyll-seo-tag coderay uglifier octopress-minify-html octokit
 gem install jekyll-assets -v 2.4.0
