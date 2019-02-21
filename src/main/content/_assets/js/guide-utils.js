e7e5dc99dff43e9da7809966946b5a2546d35c13
steven1046 <stevenzvonek1992@gmail.com>
2019-02-21 15:06:48 -0600

    Add code modal to the mobile view when clicking on hotspots

--- src/main/content/_assets/js/guide-utils.js
+++ src/main/content/_assets/js/guide-utils.js
@@ -470,6 +470,7 @@ $(document).ready(function() {
         handleFloatingTOCAccordion();
         resizeGuideSections();
         handleFloatingCodeColumn();
+        restoreCodeColumn();
     });
 
     // Check if on Apple device or Internet Explorer/Edge before enabling inertia scrolling since it doesn't work well.
