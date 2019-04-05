6be08f45813b603917223ad44cedd6c452abf576
steven1046 <stevenzvonek1992@gmail.com>
2019-04-05 11:31:48 -0500

    Fix the qa descriptor host

--- src/main/content/_assets/js/devEnvironment.js
+++ src/main/content/_assets/js/devEnvironment.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     // Check if the QA site or Dev site is loaded and add a flag to the header 
     var host = window.location.hostname;
     var label;
-    if(host.indexOf('qa-guides') > -1 || host.indexOf('draft-guides') > -1){
+    if(host.indexOf('qa-guides') > -1 || host.indexOf('draft-openlibertyio') > -1){
         label = $("<div></div>");
         label.addClass('qa-descriptor');
         label.text('QA Site');
