5b63783d8e9107e3c04181aa84eba14adfc3d549
Ellen Wyllie <Ellen.Wyllie@ibm.com>
2019-08-02 14:29:46 -0500

    Adds tags to posts, maintain state

--- src/main/content/_assets/js/blog.js
+++ src/main/content/_assets/js/blog.js
@@ -3,7 +3,7 @@ $.getJSON( "../../blog_tags.json", function(data) {
     $.each(data.blog_tags, function(j, tag) {
         // get featured tags from json
         if (tag.featured) {
-            featured_tags_html = '<p class="featured_tag" onclick="filter_posts(' + "'" + tag.name.replace(" ", "_") + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
+            featured_tags_html = '<p class="featured_tag" onclick="filterPosts(' + "'" + tag.name.replace(" ", "_") + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
             $('#featured_tags_list').append(featured_tags_html);
         }
         $(".blog_post_title_link").each(function(i, link) {
@@ -12,7 +12,7 @@ $.getJSON( "../../blog_tags.json", function(data) {
             var tags_html = "";
             if (tag.posts.indexOf(post_name) > -1) {
                 var tag_class = tag.name.replace(" ", "_");
-                tags_html = '<p class="blog_tag" onclick="filter_posts(' + "'" + tag_class + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
+                tags_html = '<p class="blog_tag" onclick="filterPosts(' + "'" + tag_class + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
 
                 $(".blog_post_content:eq(" + i + ")").addClass(tag_class);
                 $(".blog_tags_container:eq(" + i + ")").append(tags_html);
@@ -21,15 +21,73 @@ $.getJSON( "../../blog_tags.json", function(data) {
     });
 });
 
+function updateSearchUrl(tag) {
+    console.log("updating search url");
+    if (!tag) {
+        console.log("adding /blog to history");
+        // Remove query string because search text is empty
+        search_value = [location.protocol, '//', location.host, "/blog/"].join('');
+        history.pushState(null, "", search_value);
+    } else {
+        console.log("adding tag to history");
+        // Handle various search functions
+        search_value = "?search=" + tag;
+        history.pushState(null, "", search_value);
+    }
 
-function filter_posts(tag) {
-    // show filter message at top of page
-    $('#filter').show();
-    $('#filter_tag').text(tag.replace("_", " "));
-
-    // hide posts that dont have tag
-    $('.blog_post_content').hide();
-    $('#older_posts').hide();
-    $("." + tag).show();
-    $('#final_post').show();
 }
+
+function filterPosts(tag) {
+    // getting called too much
+    //updateSearchUrl(tag);
+
+    if (!tag) {
+        removeFilter();
+    }
+    else {
+        // scroll to top of page to see filter message
+        $(window).scrollTop(0);
+
+        // show filter message at top of page
+        $('#filter').show();
+        $('#filter_tag').text(tag.replace("_", " "));
+
+        // hide posts that dont have tag
+        $('.blog_post_content').hide();
+        $('#older_posts').hide();
+        $("." + tag).show();
+        $('#final_post').show();
+        updateSearchUrl(tag);
+    }
+}
+
+
+function removeFilter() {
+    updateSearchUrl();
+    $('#filter').hide();
+    $('.blog_post_content').show();
+    $('#older_posts').show();
+}
+
+$(window).on('popstate', function(){
+    var query_string = location.search;
+    if (query_string.length > 0) {
+        var tag = query_string.replace("?search=", "");
+        filterPosts(tag);
+    }
+});
+
+
+function init() {
+    var query_string = location.search;
+
+    // Process the url parameters for searching
+    if (query_string.length > 0) {
+        var tag = query_string.replace("?search=", "");
+        filterPosts(tag);
+    }
+}
+
+$(document).ready(function() {
+    init();
+});
