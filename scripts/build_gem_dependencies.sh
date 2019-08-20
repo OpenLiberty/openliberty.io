4aeabcb8d9f817c1810e14a470efe5962e7eed3f
steven1046 <stevenzvonek1992@gmail.com>
2019-08-20 14:04:54 -0500

    Set jekyll version to 3.8.6 to fix list

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,3 +1,4 @@
 echo "Installing ruby packages..."
-gem install jekyll bundler jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
+gem install bundler jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
+gem install jekyll -v 3.8.6
 gem install jekyll-assets -v 2.4.0
