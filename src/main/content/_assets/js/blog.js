b09510597d3edcc09939c0134b749d6513de7d39
Ellen Wyllie <ellen.wyllie@gmail.com>
2019-08-23 10:47:17 -0500

    Test callback

--- src/main/content/_assets/js/blog.js
+++ src/main/content/_assets/js/blog.js
@@ -1,25 +1,28 @@
 // Read tags from json file and add tag to class
-$.getJSON( "../../blog_tags.json", function(data) {
-    $.each(data.blog_tags, function(j, tag) {
-        var tag_class = tag.name.replace(" ", "_");
-        // get featured tags from json
-        if (tag.featured) {
-            featured_tags_html = '<p class="featured_tag" onclick="filterPosts(' + "'" + tag_class + "'" + '); updateSearchUrl(' + "'" + tag_class + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
-            $('#featured_tags_list').append(featured_tags_html);
-        }
-        $(".blog_post_title_link").each(function(i, link) {
-            var href = this.getAttribute('href');
-            post_name = href.substring(17).replace('.html', '');
-            var tags_html = "";
-            if (tag.posts.indexOf(post_name) > -1) {
-                tags_html = '<p class="blog_tag" onclick="filterPosts(' + "'" + tag_class + "'" + '); updateSearchUrl(' + "'" + tag_class + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
-
-                $(".blog_post_content:eq(" + i + ")").addClass(tag_class);
-                $(".blog_tags_container:eq(" + i + ")").append(tags_html);
+function getTags(callback) {
+    $.getJSON( "../../blog_tags.json", function(data) {
+        $.each(data.blog_tags, function(j, tag) {
+            var tag_class = tag.name.replace(" ", "_");
+            // get featured tags from json
+            if (tag.featured) {
+                featured_tags_html = '<p class="featured_tag" onclick="filterPosts(' + "'" + tag_class + "'" + '); updateSearchUrl(' + "'" + tag_class + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
+                $('#featured_tags_list').append(featured_tags_html);
             }
+            $(".blog_post_title_link").each(function(i, link) {
+                var href = this.getAttribute('href');
+                post_name = href.substring(17).replace('.html', '');
+                var tags_html = "";
+                if (tag.posts.indexOf(post_name) > -1) {
+                    tags_html = '<p class="blog_tag" onclick="filterPosts(' + "'" + tag_class + "'" + '); updateSearchUrl(' + "'" + tag_class + "'" + ');">' + tag.name + '</p>' + '<span>, </span>';
+
+                    $(".blog_post_content:eq(" + i + ")").addClass(tag_class);
+                    $(".blog_tags_container:eq(" + i + ")").append(tags_html);
+                }
+            });
         });
+        callback(data);
     });
-});
+}
 
 function updateSearchUrl(tag) {
     if (!tag) {
@@ -79,7 +82,9 @@ function init() {
 }
 
 $(document).ready(function() {
-    init();
+    getTags(function () {
+        init();
+    });
 
     // if blog post has no tags, add col-md-7 class so that text doesn't overlap
     $('.blog_tags_container').each(function() {
