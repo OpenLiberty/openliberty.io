0bb97621c5bbfb6329e0c5eaa081dce9cc80151a
mbroz2 <mbroz@us.ibm.com>
2019-09-13 21:29:59 -0500

    Remove direct link to latest runtime package

--- src/main/content/_assets/js/download.js
+++ src/main/content/_assets/js/download.js
@@ -12,16 +12,6 @@
 var latest_release_url = '/api/builds/latest';
 
 $(document).ready(function() {
-
-    $.ajax({
-        url: latest_release_url
-    }).done(function(data) {
-
-        $('#download_link').attr('href', data.runtime.driver_location);
-        $('#download_link_size_label').text('(' + Math.floor(data.runtime.size_in_bytes / 1048576) + ' MB)');
-
-    });
-
     // add custom css to twitter iframe
     $('#twitter_iframe_div').on('DOMSubtreeModified propertychange',"#twitter-widget-0", function() {
         $(".twitter-timeline").contents().find(".timeline-Tweet-media").css("display", "none");
