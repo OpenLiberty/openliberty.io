d3a2b62be57385b6d71b892ee85fb8d38fe8a263
steven1046 <stevenzvonek1992@gmail.com>
2019-12-17 16:24:05 -0600

    Test older version

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,4 +1,5 @@
 echo "Installing ruby packages..."
 gem install jekyll -v 3.8.6
-gem install bundler rubygems-bundler jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
+gem install bundler -v 1.7.3
+gem install jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
 gem install jekyll-assets -v 2.4.0
