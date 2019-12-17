980c72c5d2dea39b39e6887b050c5238a83bb728
steven1046 <stevenzvonek1992@gmail.com>
2019-12-17 14:58:10 -0600

    Test gem changes

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,5 +1,5 @@
 echo "Installing ruby packages..."
+bundle config --delete bin
 gem install jekyll -v 3.8.6
-gem install bundler -v 1.17.3 # Upgrading to 2.1.0 breaks the build
-gem install jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
+gem install bundler jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
 gem install jekyll-assets -v 2.4.0
