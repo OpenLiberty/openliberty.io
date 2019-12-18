cccb34ebc246faaa30edbf2f7d1bba27d143d7d3
steven1046 <stevenzvonek1992@gmail.com>
2019-12-18 08:57:00 -0600

    Try uninstalling rubygems-bundler

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,5 +1,5 @@
 echo "Installing ruby packages..."
 gem install jekyll -v 3.8.6
-gem install bundler -v 1.7.3
-gem install jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
+gem install bundler jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
 gem install jekyll-assets -v 2.4.0
+gem uninstall rubygems-bundler
