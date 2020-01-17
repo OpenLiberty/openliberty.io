644b9811415dfd05b56a9037f71fcef19ae901ce
steven1046 <stevenzvonek1992@gmail.com>
2020-01-17 16:24:15 -0600

    Upgrade maven version

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 #
 #
 sudo apt-get update
-sudo apt-get update maven
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
 sudo apt-get install python3-bs4 -y
+sudo apt-get install --only-upgrade maven
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/pipeline/.rvm/scripts/rvm
