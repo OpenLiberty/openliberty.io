96b32693f7e5c4208fedc1a931c49323eaabf687
steven1046 <stevenzvonek1992@gmail.com>
2020-01-20 10:15:47 -0600

    Update path with later maven version

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,16 @@
 sudo apt-get update
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
 sudo apt-get install python3-bs4 -y
+sudo apt-get upgrade
 # Update maven
 pushd /opt/IBM/
 sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 sudo tar -xvzf apache-maven-3.3.9-bin.tar.gz
 sudo mv apache-maven-3.3.9 maven
 popd
+export PATH=/opt/IBM/maven/apache-maven-3.3.9/bin:PATH
+echo "Maven version:"
+mvn -v
 # End update maven
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
