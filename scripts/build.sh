99382a5bb9b9f53bc4fdd812c794b3f2134caf07
steven1046 <stevenzvonek1992@gmail.com>
2020-01-21 08:54:18 -0600

    Add profile id and activation

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,22 +4,6 @@
 sudo apt-get update
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
 sudo apt-get install python3-bs4 -y
-# Update maven
-# pushd /opt/IBM/
-# sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-# echo "List contents 1:"
-# ls -l
-# sudo tar -xvzf apache-maven-3.3.9-bin.tar.gz
-# echo "List contents 2 after tar:"
-# ls -l
-# sudo mv apache-maven-3.3.9 maven
-# echo "List the maven versions in dir:"
-# ls maven
-# popd
-# export PATH=/opt/IBM/maven/apache-maven-3.3.9/bin:$PATH
-# echo "Maven version:"
-# mvn -v
-# End update maven
 
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
