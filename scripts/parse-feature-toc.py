c3eb04bfe6b15158ea63d662d2236e1661e85f36
steven1046 <stevenzvonek1992@gmail.com>
2020-02-04 13:54:55 -0600

    Update parse feature toc (3)

--- scripts/parse-feature-toc.py
+++ scripts/parse-feature-toc.py
@@ -16,6 +16,8 @@ def getTOCVersion(tocString):
        return None
 
 def createHrefNewTag(parent, tocHref, tocString):
+    # print('create new href tag')
+    # print(parent)
     hrefTag = parent.new_tag('div', href=tocHref)
     hrefTag['role'] = 'button'
     hrefTag['class'] = 'feature_version'
@@ -26,18 +28,15 @@ def createHrefNewTag(parent, tocHref, tocString):
         hrefTag.string = docVersion
     return hrefTag
 
-
 # rename the original index.html
 # os.rename('./target/jekyll-webapp/docs/build/site/feature/latest/featureOverview.html', './target/jekyll-webapp/docs/build/site/feature/latest/featureOverview.html.orig')
 
 featureIndex = BeautifulSoup(open('./target/jekyll-webapp/docs/ref/feature/latest/featureOverview.html'), "html.parser")
-# print(featureIndex)
 
 commonTOCs = {};
 # gather TOCs with version in the title
 for featureTOC in featureIndex.find_all('a', {'class': 'nav-link'}, href=True):
     toc = featureTOC.get('href')
-    # print(toc)
     pattern = re.compile('^(?P<preString>[\s\D]*)-(?P<version>\d+[.]?\d*)(?P<postString>[\s\D]*)')
     matches = pattern.match(toc)
     if matches is None:
@@ -54,28 +53,28 @@ for featureTOC in featureIndex.find_all('a', {'class': 'nav-link'}, href=True):
 commonTOCKeys = commonTOCs.keys()
 commonTOCKeys = list(commonTOCKeys)
 
-print(commonTOCKeys)
+# Keep track of new href with updated versions to update the TOCs later
+combinedPages = [];
+
+# Add to version href if we need it for linking
+antora_path = "./target/jekyll-webapp/docs/ref/feature/latest/"
 
 for commonTOC in commonTOCKeys:
     commonTOCMatchString = commonTOCs[commonTOC]
-    # matchingTitleTOCs = featureIndex.find_all('a', {'class': 'nav-link'}, href=True, string=re.compile(commonTOCMatchString)
     matchingTitleTOCs = featureIndex.find_all('a', {'class': 'nav-link'}, href=re.compile(commonTOCMatchString))
-    # print(matchingTitleTOCs)
     firstElement = True;
     # determine whether there are multiple versions
     if len(matchingTitleTOCs) > 1:
-        print(commonTOC)
         # multiple versions of the same title found, create a new html from the template
         # to put the versions at the top of the page
         featureVersionTemplate  = BeautifulSoup(open('./scripts/feature-template/common-feature-content-template.html'), "html.parser")
-        # featureTitle = featureVersionTemplate.find('div', {'class': 'page'})
         featureTitle = featureIndex.find('h1', {'class': 'page'})
+        featureTitle.string = ''
         newTOCHref = ''
         # in reverse descending order
         matchingTOCs = matchingTitleTOCs[::-1]
         for matchingTOC in matchingTOCs:
             tocHref = matchingTOC.get('href')
-            print("tocHref: " + tocHref)
             if firstElement:
                 firstElement = False
                 hrefSplits = tocHref.split('/')
@@ -87,31 +86,40 @@ for commonTOC in commonTOCKeys:
                    combineHtml = "-".join(htmlSplits) + '.html'
                    del hrefSplits[-1]
                    newTOCHref = '/'.join(hrefSplits) + '/' + combineHtml
-                   print("newTOCHref:" + newTOCHref)
+                   newTOCHref = '/docs/ref/feature/latest' + newTOCHref
+                #    print("newTOCHref:" + newTOCHref)
                    matchingTOC['href'] = newTOCHref
-                   hrefTag = createHrefNewTag(featureVersionTemplate, tocHref, matchingTOC.get('href'))
+                   combinedPages.append(newTOCHref)
+                   hrefTag = createHrefNewTag(featureIndex, tocHref, matchingTOC.get('href'))
+                #    print("matchingTOC.string: " + matchingTOC.string)
+                #    print("matchingTOC.get('href'): " + matchingTOC.get('href'))
                    hrefTag.string = matchingTOC.string
-                   print("appending version")
-                   print(hrefTag)
                    featureTitle.append(hrefTag)
-                   matchingTOC.string = commonTOC
             else:
-                hrefTag = createHrefNewTag(featureVersionTemplate, tocHref, matchingTOC.get('href'))
+                hrefTag = createHrefNewTag(featureIndex, tocHref, matchingTOC.get('href'))
+                print("appending first hrefTag: " + str(hrefTag))
                 featureTitle.append(hrefTag)
                 matchingTOC.parent.decompose()
-            print("featureTitle")
-            print(featureTitle)
+            # print("featureTitle")
+            # print(featureTitle)
         # write to the common version doc to a file
-        print(str(featureVersionTemplate))
-        # with open('./target/jekyll-webapp' +  newTOCHref, "w") as file:            
-        #     file.write(str(featureVersionTemplate))
-        with open('./target/jekyll-webapp/docs/ref/feature/latest/featureOverview.html', "w") as file:            
+        # TODO Need to just write the contents of the page over itself with the title changed to add the versions
+        with open('./target/jekyll-webapp/' + newTOCHref, "w") as file:            
             file.write(str(featureIndex))
+        # with open('./target/jekyll-webapp/docs/ref/feature/latest/featureOverview.html', "w") as file:            
+        #     file.write(str(featureIndex))
     elif len(matchingTitleTOCs) == 1:
         # single version doc is found, just strip off the version from the TOC title
         matchingTOC = matchingTitleTOCs[0]
         matchingTOC.string = commonTOC
 
+for page in combinedPages:    
+    print("page " + page)
+
+# write the new toc to each page in the antora version
+# BeautifulSoup(open('./target/jekyll-webapp/docs/ref/feature/latest/*')
+# Read in the current page and write over the TOC with the removed duplicates
+
 # write the new index.html with version control in it
-# with open('./target/jekyll-webapp/docs/build/site/feature/latest/featureOverview.html', "w") as file:
+# with open('./target/jekyll-webapp/docs/ref/feature/latest/featureOverview.html', "w") as file:
 #     file.write(str(featureIndex))
