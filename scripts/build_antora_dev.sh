84d655381cda0388e4eff7ac27d992bebd58f3d6
steven1046 <stevenzvonek1992@gmail.com>
2020-02-27 11:03:47 -0600

    Remove chain from the antora dev script

--- scripts/build_antora_dev.sh
+++ scripts/build_antora_dev.sh
@@ -1,5 +1,5 @@
 # Serve the antora docs locally for testing
 # Have the docs cloned before running this
-./scripts/build_antora_ui.sh && \
-antora --stacktrace --fetch src/main/content/docs/production-antora-playbook.yml && \
+./scripts/build_antora_ui.sh
+antora --stacktrace --fetch src/main/content/docs/production-antora-playbook.yml
 serve src/main/content/docs/build/site
