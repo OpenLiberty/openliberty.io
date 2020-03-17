7aa0f3ceafd1027306e35413714bbc006f247442
steven1046 <stevenzvonek1992@gmail.com>
2020-03-17 12:48:11 -0500

    Clone prod playbook

--- scripts/build_clone_antora_playbook.sh
+++ scripts/build_clone_antora_playbook.sh
@@ -5,7 +5,7 @@ rm -rf docs-playbook
 
 echo "Cloning the docs Antora playbook"
 # Todo specify what playbook branch to clone based on the environment
-git clone https://github.com/OpenLiberty/docs-playbook.git --branch modules
+git clone https://github.com/OpenLiberty/docs-playbook.git --branch prod
 
 # Move the docs playbook over to the docs dir so it can generate the doc pages
 cp -f docs-playbook/antora-playbook.yml src/main/content/docs/
