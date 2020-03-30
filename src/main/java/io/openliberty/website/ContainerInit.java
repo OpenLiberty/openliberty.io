8f770bf4849ca542d5e43b1ba692bc3cdbbe1ef3
Alasdair Nottingham <alasdair.nottingham@gmail.com>
2020-03-30 17:54:38 -0400

    1018 force init of the build data on startup rather than 1st request

--- src/main/java/io/openliberty/website/ContainerInit.java
+++ src/main/java/io/openliberty/website/ContainerInit.java
@@ -3,7 +3,11 @@ package io.openliberty.website;
 import java.io.IOException;
 import java.util.Set;
 
+import javax.enterprise.inject.spi.CDI;
 import javax.servlet.ServletContext;
+
+import io.openliberty.website.dheclient.DHEBuildParser;
+
 import javax.servlet.ServletContainerInitializer;
 
 public class ContainerInit implements ServletContainerInitializer {
@@ -13,5 +17,10 @@ public class ContainerInit implements ServletContainerInitializer {
     } catch (IOException e) {
       e.printStackTrace();
     }
+
+    // By initializing the DHE Build Parser and calling scheduleAsyncUpdate
+    // we start building the build list from external data sources at startup
+    // rather than on first request.
+    CDI.current().select(DHEBuildParser.class).get().scheduleAsyncUpdate();
   }
 }
