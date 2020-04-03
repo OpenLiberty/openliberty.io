c9b254da1fafaa3c333847cded9af9647c2723f0
steven1046 <stevenzvonek1992@gmail.com>
2020-04-03 14:48:13 -0500

    Add a line to antora dev script

--- scripts/build_antora_dev.sh
+++ scripts/build_antora_dev.sh
@@ -1,5 +1,6 @@
 # Serve the antora docs locally for testing
 # Have the docs cloned before running this
 ./scripts/build_antora_ui.sh
+./scripts/build_clone_antora_playbook.sh
 antora --stacktrace --fetch src/main/content/docs/antora-playbook.yml
 serve src/main/content/docs/build/site
