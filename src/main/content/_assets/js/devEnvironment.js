b7242ab3b58778e34f382221ebc5e855a244fa40
Ellen Wyllie <ellen.wyllie@gmail.com>
2020-04-27 17:10:18 -0500

    Mobile and tablet collapsible nav, remove header.js

--- src/main/content/_assets/js/devEnvironment.js
+++ src/main/content/_assets/js/devEnvironment.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
     // Check if the QA site or Dev site is loaded and add a flag to the header 
     var host = window.location.hostname;
-    var label;
-    if(host.indexOf('qa-guides') > -1 || host.indexOf('draft-openlibertyio') > -1){
-        label = $("<div></div>");
-        label.addClass('qa-descriptor');
+    var label = $("<div></div>");
+    label.addClass('qa-descriptor');
+    if (host.indexOf('qa-guides') > -1) {
         label.text('QA Site');
-        $(".navbar").after(label);
-    } else if(host.indexOf('openlibertydev') > -1){
-        label = $("<div></div>");
-        label.addClass('qa-descriptor');
+        $("#nav_bar").after(label);
+    } else if (host.indexOf('draft-openlibertyio') > -1) {
+        label.text('Draft Site');
+        $("#nav_bar").after(label);
+    } else if (host.indexOf('dev-openlibertyio') > -1) {
         label.text('Dev Site');
-        $(".navbar").after(label);
+        $("#nav_bar").after(label);
+    } else if (host.indexOf('demo') > -1) {
+        label.text('Demo Site');
+        $("#nav_bar").after(label);
     }
 });
