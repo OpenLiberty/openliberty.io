3e7d4baeb28f0995a0b557ebc67cc195d4b841e6
steven1046 <stevenzvonek1992@gmail.com>
2020-05-14 16:33:07 -0500

    Install jekyll-include-cache plugin

--- scripts/build_gem_dependencies.sh
+++ scripts/build_gem_dependencies.sh
@@ -1,5 +1,5 @@
 echo "Installing ruby packages..."
 gem install jekyll -v 3.8.6
-gem install bundler jekyll-feed jekyll-asciidoc coderay uglifier octopress-minify-html octokit
+gem install bundler jekyll-feed jekyll-asciidoc jekyll-include-cache coderay uglifier octopress-minify-html octokit
 gem install jekyll-assets -v 2.4.0
 gem uninstall -i /usr/local/rvm/gems/ruby-2.4.1@global rubygems-bundler
