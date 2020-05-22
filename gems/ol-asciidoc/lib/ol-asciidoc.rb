d9b977d51368ce79bf0a96e025995c3b48d70d64
steven1046 <stevenzvonek1992@gmail.com>
2020-05-22 16:49:11 -0500

    Fix the ol-asciidoc gem

---
+++ gems/ol-asciidoc/lib/ol-asciidoc.rb
@@ -0,0 +1,36 @@
+require 'asciidoctor'
+require 'asciidoctor/extensions'
+
+class FeatureLinkMacro < Asciidoctor::Extensions::InlineMacroProcessor
+  use_dsl
+  named :feature
+
+  def process parent, target, attrs
+
+    (create_anchor parent, target, type: :link, target: "/docs/ref/feature/#" + target + ".html").convert
+  end
+end
+
+class ConfigLinkMacro < Asciidoctor::Extensions::InlineMacroProcessor
+  use_dsl
+  named :config
+
+  def process parent, target, attrs
+    (create_anchor parent, target, type: :link, target: "/docs/ref/config/#" + target + ".html").convert
+  end
+end
+
+class JavadocLinkMacro < Asciidoctor::Extensions::InlineMacroProcessor
+  use_dsl
+  named :javadoc
+
+  def process parent, target, attrs
+    target
+  end
+end
+
+Asciidoctor::Extensions.register do
+  inline_macro FeatureLinkMacro
+  inline_macro ConfigLinkMacro
+  inline_macro JavadocLinkMacro
+end
