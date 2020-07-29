76794b4a9f388248e752b0cfa3ded2492f69cde4
steven1046 <stevenzvonek1992@gmail.com>
2020-07-29 18:38:53 -0500

    Remove title from the query params and text on the docs 404 page

--- src/main/content/antora_ui/src/js/09-noversion.js
+++ src/main/content/antora_ui/src/js/09-noversion.js
@@ -1,12 +1,6 @@
 $(window).on("load", function(){
     $.ready.then(function(){
-        var path = window.location.search;
-        var page_index = path.indexOf('?page=');
-        if(page_index > -1){
-            var page_name = decodeURIComponent(path.substring(page_index + 6));
-
-            var version = $('.context .version').text();
-            $('.doc > .paragraph').text("The page " + page_name + " does not exist in the " + version + " version of the documentation.");
-        }        
+        var version = $('.context .version').text();
+        $('.doc .paragraph').text("The requested page does not exist in the " + version + " version of the documentation.");               
     });
 });
