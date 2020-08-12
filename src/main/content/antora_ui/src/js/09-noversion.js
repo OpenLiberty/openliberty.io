52eda6f638a6ec7955d32251d606a2677acb5f82
steven1046 <stevenzvonek1992@gmail.com>
2020-08-12 14:33:31 -0500

    Change page to document on the doc 404 page

--- src/main/content/antora_ui/src/js/09-noversion.js
+++ src/main/content/antora_ui/src/js/09-noversion.js
@@ -1,6 +1,6 @@
 $(window).on("load", function(){
     $.ready.then(function(){
         var version = $('.context .version').text();
-        $('.doc .paragraph').text("The requested page does not exist in the " + version + " version of the documentation.");               
+        $('.doc .paragraph').text("The requested document does not exist in the " + version + " version of the documentation.");               
     });
 });
