1e0e0d7fd23e2d49c5467fc7af83530960cc6c70
Ellen Wyllie <ellen.wyllie@gmail.com>
2020-08-27 09:36:05 -0500

    Improvements on mobile guides

--- src/main/content/_assets/js/openliberty.js
+++ src/main/content/_assets/js/openliberty.js
@@ -10,6 +10,20 @@
  *******************************************************************************/
 
 $(document).ready(function() {
+    // if (typeof inSingleColumnView === "function") {
+    //     console.log("isSingleColView is a function");
+    //     if(inSingleColumnView()) {
+    //         console.log("SingleColView");
+    //     }
+    //     else {
+    //         console.log('too big');
+    //     }
+    // }
+    // else {
+    //     console.log("isSingleColView is NOT a function");
+
+    // }
+
 	var prevScrollTop = 0;
 	$(window).scroll(function () {
         var currScrollTop = $(this).scrollTop();
@@ -57,13 +71,30 @@ $(document).ready(function() {
 });
 
 function showNav() {
+    var nav_height = $("#nav_bar").outerHeight();
     $("#nav_bar").addClass("fixed_top");
-    $("#toc_column").css("top", "60px");
-    $('body').css("margin-top", "60px");
+    $("#toc_column").css("top", nav_height + "px");
+    $("#toc_indicator").css("top", nav_height + "px");
+
+    $('body').css("margin-top", nav_height + "px");
     // $("#guide_column").css("top", "120px");
     // $("#guides_container").css({"position":"relative", "top":"60px"});
-    $("#code_column").css({"position":"fixed", "top":"60px"})
-    $("#toc_inner").css("margin-top", "60px");
+    $("#code_column").css({"position": "fixed", "top": nav_height + "px"})
+    $("#toc_inner").css("margin-top", nav_height + "px");
+
+
+    if ($("#mobile_toc_accordion_container").hasClass("fixed_toc_accordion")) {
+        console.log("accordion has fixed class");
+        var accordion_height = $("#mobile_toc_accordion_container").outerHeight();
+        
+        $("#nav_bar").css("top", "0px");
+        $("#mobile_toc_accordion_container").css("top", accordion_height + "px");
+
+    }
+    else {
+        console.log("no class");
+    }
+
 }
 
 function hideNav() {
@@ -71,8 +102,12 @@ function hideNav() {
     $("#nav_bar").css({"top": "-60px"});
     $('body').css("margin-top", "0px");
     $("#toc_column").css("top", "0px");
+    $("#toc_indicator").css("top", "0px");
     // $("#guide_column").css("top", "0px");
     // $("#guides_container").css({"position":"", "top":""});
     $("#code_column").css({"position":"fixed", "top":"0px"})
     $("#toc_inner").css("margin-top", "0px");
+
+    $("#mobile_toc_accordion_container").css("top", "0px");
+
 }
