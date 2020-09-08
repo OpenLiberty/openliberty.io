9f9c792094c63e84f3bd1c9e5be26baefdd536c6
steven1046 <stevenzvonek1992@gmail.com>
2020-09-08 08:48:37 -0500

    Remove cf-push-2 and use parameters for what space to deploy to

--- .travis/cf-push.sh
+++ .travis/cf-push.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+BLUEMIX_ORG="$1"
+BLUEMIX_USER="$2"
+BLUEMIX_PASS="$3"
+
 echo "============== INSTALLING CLOUD FOUNDRY CLI CLIENT =============="
 # https://github.com/cloudfoundry/cli/releases
 wget --max-redirect=1 --output-document=cf_cli_6.35.0.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.35.0&source=github-rel"
@@ -8,7 +12,7 @@ gunzip cf_cli_6.35.0.tgz
 tar -xvf cf_cli_6.35.0.tar
 
 echo "============== LOGGING INTO CLOUD FOUNDRY =============="
-./cf login -a=$BLUEMIX_API -s=$BLUEMIX_SPACE -o=$BLUEMIX_ORGANIZATION -u=$BLUEMIX_USER -p=$BLUEMIX_PASSWORD
+./cf login -a=$BLUEMIX_API -s=$BLUEMIX_SPACE -o=$BLUEMIX_ORG -u=$BLUEMIX_USER -p=$BLUEMIX_PASS
 
 # ==== VARIABLE SETUP ====
 APP=`echo $ROUTE | sed -e 's,\..*,,'`
