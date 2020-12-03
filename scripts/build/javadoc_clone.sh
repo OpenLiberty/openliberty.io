794c19a5be98fa337256a3a3d4d0628f98f72c28
steven1046 <stevenzvonek1992@gmail.com>
2020-12-03 14:49:42 -0600

    Copy javadoc rm into javadoc_clone.sh

--- scripts/build/javadoc_clone.sh
+++ scripts/build/javadoc_clone.sh
@@ -18,6 +18,8 @@ echo "Cloning the $BRANCH_NAME branch of javadoc repository..."
 
 # Clone docs-javadoc repo
 pushd src/main/content
+# Remove previous installations of docs-javadoc
+rm -rf docs-javadoc
 git clone https://github.com/OpenLiberty/docs-javadoc.git --branch $BRANCH_NAME
 popd
 
