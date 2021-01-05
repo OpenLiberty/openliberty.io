d53c56c29558ed6ceea60a5fb610554b808e75ec
steven1046 <stevenzvonek1992@gmail.com>
2021-01-05 09:24:22 -0600

    Upgrade ruby to 2.5.7

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,8 +13,8 @@ cat $SCRIPT_DIR/gpg/pkuczynski.asc | gpg --import -
 curl -sSL https://get.rvm.io | bash -s stable
 source /usr/local/rvm/scripts/rvm
 rvm requirements
-rvm install 2.4.1
-rvm use 2.4.1 --default
+rvm install 2.5.7
+rvm use 2.5.7 --default
 
 # Print out default locale and change it to en_US.UTF-8
 echo "Default locale:"
