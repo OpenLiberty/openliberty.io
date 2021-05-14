ae053f2f70ecbe5e09d01d2e7208f58517c4653b
steven1046 <stevenzvonek1992@gmail.com>
2021-05-14 10:52:50 -0500

    Add environment production for minification gem

--- scripts/jekyll_serve_dev.sh
+++ scripts/jekyll_serve_dev.sh
@@ -1,1 +1,1 @@
-jekyll s --host 0.0.0.0 --future --source src/main/content --config src/main/content/_config.yml,src/main/content/_dev_config.yml --drafts
+JEKYLL_ENV=production jekyll s --host 0.0.0.0 --future --source src/main/content --config src/main/content/_config.yml,src/main/content/_dev_config.yml --drafts
