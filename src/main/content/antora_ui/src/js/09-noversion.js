3f192dfc48f3af8bce003f33be1ab9054b5ca181
Natalie Bernhard <natalie.bernhard@ibm.com>
2021-06-07 17:00:55 -0400

    initial changes for this functionality

--- src/main/content/antora_ui/src/js/09-noversion.js
+++ src/main/content/antora_ui/src/js/09-noversion.js
@@ -1,6 +1,21 @@
-$(window).on("load", function(){
-    $.ready.then(function(){
-        var version = $('.context .version').text();
-        $('.doc .paragraph').text("The requested document does not exist in the " + version + " version of the documentation.");               
-    });
+$(window).on("load", function() {
+  $.ready.then(function() {
+    var attempted = document.referrer;
+    var doc = attempted.substring(attempted.lastIndexOf("/") + 1);
+    doc = doc.substring(0, doc.indexOf(".html"));
+    console.log(doc);
+    var versions = [];
+    $(".components .versions .version")
+      .find("a")
+      .map(() => {
+        versions.push($(this).text());
+      });
+    console.log(versions);
+    var version = $(".context .version").text();
+    $(".doc .paragraph").text(
+      "The requested document does not exist in the " +
+        version +
+        " version of the documentation."
+    );
+  });
 });
