4f1021b417df2796e13f1f0fbc356207efc841a5
Natalie Bernhard <natalie.bernhard@ibm.com>
2021-06-08 17:04:55 -0400

    noversion to noversion fix

--- src/main/content/antora_ui/src/js/09-noversion.js
+++ src/main/content/antora_ui/src/js/09-noversion.js
@@ -1,10 +1,28 @@
 $(window).on("load", function() {
   $.ready.then(function() {
     if (!document.referrer.includes("noversion.html")) {
+      var folder = "ROOT";
+      var dir = "";
       //get info about doc that was attempted to be reached
       var attempted = document.referrer;
       var doc = attempted.substring(attempted.lastIndexOf("/") + 1);
       doc = doc.substring(0, doc.indexOf(".html"));
+
+      var preceed1 = attempted.substring(0, attempted.lastIndexOf("/"));
+      preceed1 = preceed1.substring(preceed1.lastIndexOf("/") + 1);
+      if (preceed1 === "reference") {
+        folder = "reference";
+      } else {
+        if (
+          preceed1 === "feature" ||
+          preceed1 === "javadoc" ||
+          preceed1 === "config" ||
+          preceed1 === "command"
+        ) {
+          folder = "reference";
+          dir = "/" + preceed1;
+        }
+      }
 
       //get all current versions of docs
       var versions = [];
@@ -23,7 +41,11 @@ $(window).on("load", function() {
             Accept: "application/vnd.github.v3.raw"
           },
           url:
-            "https://api.github.com/repos/OpenLiberty/docs/contents/modules/ROOT/pages?ref=v" +
+            "https://api.github.com/repos/OpenLiberty/docs/contents/modules/" +
+            folder +
+            "/pages" +
+            dir +
+            "?ref=v" +
             v,
           type: "GET",
           success: function(response) {
@@ -36,6 +58,8 @@ $(window).on("load", function() {
                     "/docs/" +
                     v +
                     "/" +
+                    (folder === "reference" ? "reference/" : "") +
+                    (dir !== "" ? dir + "/" : "") +
                     doc +
                     ".html>v" +
                     v +
@@ -60,6 +84,8 @@ $(window).on("load", function() {
       $(".doc .paragraph").append(
         "<div><p>Below are links to the requested document in other versions of the documentation.</p></div><ul></ul>"
       );
+    } else {
+      $(".doc .paragraph div p").remove();
     }
   });
 });
