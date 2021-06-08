d21d846713981b6c216fa1dffe08d13a8aca6b8d
Steven Zvonek <szvonek@us.ibm.com>
2021-06-08 14:08:16 -0500

    Move noindex before the antora ui build

--- scripts/build_antora_dev.sh
+++ scripts/build_antora_dev.sh
@@ -5,10 +5,10 @@
 # Install http-server for local site viewing
 npm i -g http-server
 
-./scripts/build/antora_install.sh
-
 # add noindex metdata for non prod sites
 cp src/main/content/_includes/noindex.html src/main/content/antora_ui/src/partials/noindex.hbs
+
+./scripts/build/antora_install.sh
 
 # Run with the --update flag to fetch the latest doc changes
 if [[ $* == *--update* ]]; then
