cddcd947024b59999b8fb3c33656e381deba026f
Natalie Bernhard <natalie.bernhard@ibm.com>
2021-08-10 15:51:24 -0400

    added java ee url handling

--- src/main/content/_assets/js/javadoc-redirect.js
+++ src/main/content/_assets/js/javadoc-redirect.js
@@ -21,5 +21,27 @@ $(function () {
       pack +
       "/package-frame.html&class=" +
       jd;
+  } else if (
+    window.top.location.href.includes("/docs/modules/reference/liberty-javaee")
+  ) {
+    var jd = window.top.location.href;
+    var version = jd.substring(
+      jd.indexOf("liberty-javaee") + 14,
+      jd.indexOf("liberty-javaee") + 15
+    );
+    jd = jd.substring(jd.indexOf("liberty-javaee") + 24);
+    var pack = jd.substring(0, jd.lastIndexOf("/"));
+    var port =
+      window.top.location.port !== "" ? ":" + window.top.location.port : "";
+    window.top.location.href =
+      "https://" +
+      window.top.location.hostname +
+      port +
+      "/docs/latest/reference/javadoc/liberty-javaee" +
+      version +
+      "-javadoc.html#package=" +
+      pack +
+      "/package-frame.html&class=" +
+      jd;
   }
 });
