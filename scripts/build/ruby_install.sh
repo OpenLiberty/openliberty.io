6238aa466c8a11e7a965c7fbc3a919c53ddd8f8e
Steven Zvonek <szvonek@us.ibm.com>
2021-09-30 14:15:05 -0500

    Change curl command to point at rvm installer instead of rvm.io

--- scripts/build/ruby_install.sh
+++ scripts/build/ruby_install.sh
@@ -7,7 +7,7 @@ echo "Install Ruby & required packages/gems"
 cat $BUILD_SCRIPTS_DIR/../gpg/mpapis.asc | gpg --import -
 cat $BUILD_SCRIPTS_DIR/../gpg/pkuczynski.asc | gpg --import -
 
-curl -v -k -sSL https://get.rvm.io | bash -s stable
+curl -v -k -sSL https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer | bash -s stable
 set +e
 source /usr/local/rvm/scripts/rvm || true
 set -e
