b8ca44976cc0718e32f266501910d8c2b848bca5
Kin Ueng <github@kinueng.com>
2022-01-07 11:21:22 -0600

    New PACKAGE_SIGNATURE_LOCATIONS constant

--- src/main/java/io/openliberty/website/Constants.java
+++ src/main/java/io/openliberty/website/Constants.java
@@ -36,6 +36,7 @@ public final class Constants {
     public static final String TESTS_LOG = "tests_log";
     public static final String DRIVER_LOCATION = "driver_location";
     public static final String PACKAGE_LOCATIONS = "package_locations";
+    public static final String PACKAGE_SIGNATURE_LOCATIONS = "package_signature_locations";
     public static final String RUNTIME_RELEASES = "runtime_releases";
     public static final String RUNTIME_NIGHTLY_BUILDS = "runtime_nightly_builds";
     public static final String RUNTIME_BETAS = "runtime_betas";
