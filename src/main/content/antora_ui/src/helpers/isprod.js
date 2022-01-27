4db8267d4d585460f6609987606b2793c79c203c
Annie Nguyen <aknguyen@us.ibm.com>
2022-01-27 14:58:53 -0600

    test code in draft

--- src/main/content/antora_ui/src/helpers/isprod.js
+++ src/main/content/antora_ui/src/helpers/isprod.js
@@ -1,16 +1,17 @@
 'use strict'
 
-const { posix: path } = require('path')
-
-function isprod(site) {
-  console.log('prod env var', process.env.PROD_SITE);
-  let host = site.homeUrl;
-  console.log('site.homeUrl', host);
-  if (host.indexOf('openliberty.io') !== -1) {
-    console.log('isProduction', true);
-    return true;
+function isprod(siteUrl) {
+  console.log('site', siteUrl);
+  //console.log('prod env var', process.env.PROD_SITE);
+  if (siteUrl) {
+    let host = siteUrl;
+    //console.log('site.homeUrl', host);
+    if (host.indexOf('openliberty.io') !== -1) {
+      console.log('isProduction', true);
+      return true;
+    }
   }
   return false;
-}
+};
 
 module.exports = isprod
