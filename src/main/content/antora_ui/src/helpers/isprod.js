f37baee30e842edda8f41ed5bbaa4fc86ffe8816
Annie Nguyen <aknguyen@us.ibm.com>
2022-01-31 14:58:40 -0600

    test env var

--- src/main/content/antora_ui/src/helpers/isprod.js
+++ src/main/content/antora_ui/src/helpers/isprod.js
@@ -1,16 +1,19 @@
 'use strict'
 
-function isprod(env) {
-  console.log('env', env);
-  if (env) {
-    let isProd = env.get("PROD_SITE");
-    console.log('isProduction', isProd);
-    if (isProd) {
+function isprod(site) {
+  console.log('site', site);
+  if (site) {
+    let host = site.homeUrl;
+    console.log('site.homeUrl', host);
+    console.log('site.url', site.url);
+    console.log('site.ui.url', site.ui.url);
+    //console.log('prod env var', process.env.PROD_SITE);
+    if (host.indexOf('openliberty.io') !== -1) {
+      console.log('isProduction', true);
       return true;
     }
   }
   return false;
 };
-//The map of environment variables
 
 module.exports = isprod
