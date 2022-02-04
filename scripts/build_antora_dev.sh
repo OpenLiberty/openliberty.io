6b88658f9dc74f09f23d7ec9eb376328cd498efc
Kin Ueng <github@kinueng.com>
2022-02-04 10:11:24 -0600

    Revert "2218"

--- scripts/build_antora_dev.sh
+++ scripts/build_antora_dev.sh
@@ -5,6 +5,9 @@
 # Install http-server for local site viewing
 npm i -g http-server
 
+# add noindex metdata for non prod sites
+cp src/main/content/_includes/noindex.html src/main/content/antora_ui/src/partials/noindex.hbs
+
 ./scripts/build/antora_install.sh
 
 # Run with the --update flag to fetch the latest doc changes
