d21948dabc3511d934c23d5a7aedc317d65c1653
Annie Nguyen <aknguyen@us.ibm.com>
2022-02-07 14:31:07 -0600

    playing with prod var

--- src/main/content/antora_ui/src/helpers/isProd.js
+++ src/main/content/antora_ui/src/helpers/isProd.js
@@ -3,7 +3,8 @@
 function isProd() {
   console.error('checking if prod', (prod));
   //console.error('process.env', process.env);
-  console.error('prod.env', prod.data.root.env);
+  //console.error('prod.env', prod.data.root.env);
+  console.error('hostname', location.hostname);
   let prodSite = prod.data.root.env.PROD_SITE;
   console.error('prod.env', prodSite);
   if (prodSite === true) {
