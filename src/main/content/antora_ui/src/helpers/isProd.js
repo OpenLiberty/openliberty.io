abce9835cb78ba8073a1a7335b5f56b878254218
Annie Nguyen <aknguyen@us.ibm.com>
2022-02-08 16:10:12 -0600

    test env var

--- src/main/content/antora_ui/src/helpers/isProd.js
+++ src/main/content/antora_ui/src/helpers/isProd.js
@@ -4,10 +4,11 @@ function isProd(env) {
   console.error('env', env);
   var isProduction = env.PROD_SITE;
   console.error('env PROD_SITE', isProduction);
-  if (isProduction === true) {
+  if (isProduction) {
     console.error('isProd', true);
     return true;
   }
+  console.error('isProd', false);
   return false;
 };
 
