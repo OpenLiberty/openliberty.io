143454f73985d9f07667e0ee9908a74a80254805
Annie Nguyen <aknguyen@us.ibm.com>
2022-02-08 11:53:40 -0600

    test env var

--- src/main/content/antora_ui/src/helpers/isProd.js
+++ src/main/content/antora_ui/src/helpers/isProd.js
@@ -1,14 +1,18 @@
 'use strict'
 
-function isProd(prod) {
-  console.error('checking if prod', (prod));
-  console.error('process.env', process.env.PROD_SITE);
-  //console.error('prod.env', prod.data.root.env);
-  console.error('hostname', window.location.hostname);
-  console.error('this hostname', this.hostname);
-  let prodSite = prod.data.root.env.PROD_SITE;
-  console.error('prod.env', prodSite);
-  if (prodSite === true) {
+const Handlebars = require('handlebars')
+
+Handlebars.registerHelper('prod', () => {
+  console.error('env', process.env);
+  console.error('prod', process.env.PROD_SITE);
+  return process.env.PROD_SITE === true;
+})
+
+function isProd(env) {
+  console.error('env', env);
+  isProd = env.PROD_SITE;
+  console.error('env PROD_SITE', isProd);
+  if (isProd === true) {
     console.error('isProd', true);
     return true;
   }
