3d50f3c82808493e6e4e6dd1a9a55100f95aa952
Kin Ueng <github@kinueng.com>
2022-02-17 13:11:29 -0600

    Use Antora CLI option `--key`

--- scripts/build/antora_build_docs.sh
+++ scripts/build/antora_build_docs.sh
@@ -2,7 +2,7 @@ echo "Using the Antora playbook to generate what content to display for docs"
 if [ "$PROD_SITE" = true ]
   then    
     # Enable google analytics in docs
-    antora --fetch --stacktrace --google-analytics-key=GTM-TKP3KJ7 src/main/content/docs/antora-playbook.yml
+    antora --fetch --stacktrace --key=GTM-TKP3KJ7 src/main/content/docs/antora-playbook.yml
   else
     antora --fetch --stacktrace src/main/content/docs/antora-playbook.yml
 fi
