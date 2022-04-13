f324c284ba784580d2d858df8f5a414851561933
Sreejith K S <Sreejith.K.S@ibm.com>
2022-04-13 15:41:45 +0530

    test

--- src/main/content/antora_ui/src/js/08-links.js
+++ src/main/content/antora_ui/src/js/08-links.js
@@ -1,5 +1,6 @@
 $(window).on("load", function(){
     $.ready.then(function(){
+        console.log("its changed");
        $("a").each(function(){
            if(this.hostname !== location.hostname){
                this.target = "_blank";
