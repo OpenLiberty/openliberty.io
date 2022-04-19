45ae2f5c790b70b2e1b2333eeda12fe0ef9aedf6
Sreejith K S <Sreejith.K.S@ibm.com>
2022-04-19 16:12:48 +0530

    test

--- src/main/content/antora_ui/src/js/12-canonical.js
+++
@@ -1,9 +0,0 @@
-console.log("12-canonical.js loaded");
-var link = document.createElement('link');
-link.rel = 'canonical';
-var url = "https://demo2-openlibertyio.mybluemix.net/"+(window.location.pathname+window.location.search).substr(1)
-url = url.split('/');
-url[4] = 'latest';
-url = url.join('/')
-link.href = url;
-document.head.appendChild(link);
