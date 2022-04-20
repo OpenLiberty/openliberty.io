f84c55c14413edf4ceb2599a63cee7c2c68faebf
Annie Nguyen <aknguyen@us.ibm.com>
2022-04-20 14:02:56 -0500

    add more tests for OL Starter

---
+++ src/test/cypressjs/scripts/buildGradle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+downloadsDir=$1
+echo "JAVA_HOME=$JAVA_HOME"
+echo cd ${downloadsDir}
+cd ${downloadsDir}
+echo pwd
+pwd
+echo ls
+ls
+echo ./gradlew libertyDev
+./gradlew libertyDev > ${downloadsDir}/output.txt 2>&1 &
+exit 0
