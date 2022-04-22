774cbd2b2610ae95d89036effd7ce3957206a6b4
Sreejith K S <Sreejith.K.S@ibm.com>
2022-04-22 11:30:59 +0530

    test

--- src/main/content/_assets/js/guides.js
+++ src/main/content/_assets/js/guides.js
@@ -10,7 +10,7 @@
  *******************************************************************************/
 
 $(document).ready(function () {
-  console.log("guildes 123 new1 js loaded");
+  console.log("guildes 22Apr");
   var title_key = 1;
   var description_key = 2;
   var tags_key = 4;
