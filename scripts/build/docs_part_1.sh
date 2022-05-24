635cbc7b0991bc9b06a8b7e3117bfdbb5c034170
Annie Nguyen <aknguyen@us.ibm.com>
2022-05-24 17:16:28 -0500

    test website staging - debug5

--- scripts/build/docs_part_1.sh
+++ scripts/build/docs_part_1.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-#comment this below line in local if nvm already installed
-RUN echo "Install Node"
-
-# Cannot find a way to set the NODE_VERSION based on the version installed by `nvm install --lts`
-ENV NODE_VERSION="v16.13.2"
-ENV NVM_DIR=/root/.nvm
-RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-RUN . "$NVM_DIR/nvm.sh" && nvm install --lts
-RUN . "$NVM_DIR/nvm.sh" && node --version
-RUN . "$NVM_DIR/nvm.sh" && npm --version
-ENV PATH="/root/.nvm/versions/node/${NODE_VERSION}/bin/:${PATH}"
-RUN node --version
-RUN npm --version
-
 set -e
 export BUILD_SCRIPTS_DIR=$(dirname $0)
 echo "BUILD_SCRIPTS_DIR: $BUILD_SCRIPTS_DIR"
@@ -25,7 +11,7 @@ echo "BUILD_SCRIPTS_DIR: $BUILD_SCRIPTS_DIR"
 #$BUILD_SCRIPTS_DIR/antora_install.sh
 # Install Antora on the machine
 echo "Install Antora"
-RUN npm i -g @antora/cli@3.0.1
+npm i -g @antora/cli@3.0.1
 
 $BUILD_SCRIPTS_DIR/antora_build_ui.sh
 
