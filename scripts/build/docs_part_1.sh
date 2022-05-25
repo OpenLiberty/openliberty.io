d5349485d06fc4b3c4654cf877aeb32700927499
Annie Nguyen <aknguyen@us.ibm.com>
2022-05-25 10:32:33 -0500

    test website staging - debug7

--- scripts/build/docs_part_1.sh
+++ scripts/build/docs_part_1.sh
@@ -7,6 +7,14 @@ echo "BUILD_SCRIPTS_DIR: $BUILD_SCRIPTS_DIR"
 echo "Begin building of Antora portion of docs"
 
 #$BUILD_SCRIPTS_DIR/node_install.sh
+echo "Install Node"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install --lts
+
+node --version
+echo "npm version:"
+echo `npm -v`
 
 $BUILD_SCRIPTS_DIR/antora_install.sh
 
