a017bc475d6a199a325f5ffffb82dd619e36c947
Annie Nguyen <aknguyen@us.ibm.com>
2022-05-25 16:56:40 -0500

    test website staging - node install script

--- scripts/build/node_install.sh
+++ scripts/build/node_install.sh
@@ -3,13 +3,19 @@
 timer_start=$(date +%s)
 
 echo "Install Node"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install --lts
-
-echo "npm version:"
-echo `npm -v`
+#comment this below line in local if nvm already installed
+# Cannot find a way to set the NODE_VERSION based on the version installed by `nvm install --lts`
+export NODE_VERSION="v16.15.0"
+export NVM_DIR=/root/.nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+. "$NVM_DIR/nvm.sh" && nvm install --lts
+. "$NVM_DIR/nvm.sh" && nvm alias default node
+. "$NVM_DIR/nvm.sh" && nvm use --lts
+export PATH="/root/.nvm/versions/node/${NODE_VERSION}/bin/:${PATH}"
+node --version
+npm --version
+echo "npm analysis during build"
+npm ls -g --depth=0
 
 timer_end=$(date +%s)
 echo "Total execution time for installing Node: '$(date -u --date @$(( $timer_end - $timer_start )) +%H:%M:%S)'"
