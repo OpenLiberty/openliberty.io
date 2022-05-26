e92e018ab97863626902dac40a012898b2ab6ff7
Annie Nguyen <aknguyen@us.ibm.com>
2022-05-26 11:59:25 -0500

    test website staging - test export node var

--- scripts/build/node_install.sh
+++ scripts/build/node_install.sh
@@ -5,12 +5,15 @@ timer_start=$(date +%s)
 echo "Install Node"
 #comment this below line in local if nvm already installed
 # Cannot find a way to set the NODE_VERSION based on the version installed by `nvm install --lts`
-export NODE_VERSION="v16.15.0"
+#export NODE_VERSION="v16.15.0"
 export NVM_DIR=/root/.nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . "$NVM_DIR/nvm.sh" && nvm install --lts
 . "$NVM_DIR/nvm.sh" && nvm alias default node
 . "$NVM_DIR/nvm.sh" && nvm use --lts
+. "$NVM_DIR/nvm.sh" && export NODE_VERSION=$(node --version) && echo "$NODE_VERSION"
+#ENV NODE_VERSION="v16.15.0"
+echo "$NODE_VERSION"
 export PATH="/root/.nvm/versions/node/${NODE_VERSION}/bin/:${PATH}"
 node --version
 npm --version
