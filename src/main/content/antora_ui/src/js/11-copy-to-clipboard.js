27fedb40198fe6b9c5f3ecfd1ea6741f07060e91
Natalie Bernhard <natalie.bernhard@ibm.com>
2023-02-08 12:02:12 -0600

    functioning for blogs and docs

--- src/main/content/antora_ui/src/js/11-copy-to-clipboard.js
+++ src/main/content/antora_ui/src/js/11-copy-to-clipboard.js
@@ -11,44 +11,34 @@
 
  var code_blocks_with_copy_to_clipboard = 'pre:not(.no_copy pre)'; // CSS Selector
 $(document).ready(function () {
-    // Show copy to clipboard button when mouse enters code block only if block lacks .no_copy class
-    $(code_blocks_with_copy_to_clipboard).on('mouseenter', function(event) {
-        target = $(event.currentTarget);
-        $('main').append('<div id="copied_confirmation">Copied to clipboard</div><img id="copy_to_clipboard" src="../../../../_/img/guides_copy_button.svg" alt="Copy code block" title="Copy code block">');
-        $('#copy_to_clipboard').css({
-            top: target.offset().top + 1,
-            right: $(window).width() - (target.offset().left + target.outerWidth()) + 1
-        }).stop().fadeIn();
-    // Hide copy to clipboard button when mouse leaves code block (unless mouse enters copy to clipboard button)
-    }).on('mouseleave', function(event) {
-        var x = event.clientX;
-        var y = event.clientY + $(window).scrollTop();
-        var copy_button_top = $('#copy_to_clipboard').offset().top;
-        var copy_button_left = $('#copy_to_clipboard').offset().left;
-        var copy_button_bottom = copy_button_top + $('#copy_to_clipboard').outerHeight();
-        var copy_button_right = $('#copy_to_clipboard').offset().left + $('#copy_to_clipboard').outerWidth();
-        
-        if(!(x > copy_button_left
-            && x < copy_button_right	
-            && y > copy_button_top	
-            && y < copy_button_bottom)) {
-            $('#copied_confirmation').remove();
-            $('#copy_to_clipboard').remove();
-            $('#copy_to_clipboard').stop().fadeOut();
-        }
-
+    $(code_blocks_with_copy_to_clipboard).each(function (){
+        $(this).wrap('<div class="code_block_wrapper" title="Code block"></div>');  
+    })
+    $('.code_block_wrapper').each(function (){
+        $(this).prepend('<div id="copied_confirmation">Copied to clipboard</div><input type="image" id="copy_to_clipboard" src="/img/guides_copy_button.svg" alt="Copy code block" title="Copy code block"/>');
     });
+    $(code_blocks_with_copy_to_clipboard)
+        .each(function (event) {
+            console.log($(this).siblings());
+            $(this).siblings('#copy_to_clipboard')
+                .css({
+                    "top": "2px",
+                    "right": "2px",
+                })
+            $(this).siblings('#copied_confirmation')
+                .css({
+                    "top": "-15px",
+                    "right": "0px",
+                })
+    })
 
     // Copy target element and show copied confirmation when copy to clipboard button clicked
     $(document).on("click", "#copy_to_clipboard", function(event) {
         event.preventDefault();
         // Target was assigned while hovering over the element to copy.
-        copy_element_to_clipboard(target, function(){
-            $('#copied_confirmation').css({	
-                top: target.offset().top - 15,
-                right: $(window).width() - (target.offset().left + target.outerWidth()) + 1
-            }).stop().fadeIn().delay(3500).fadeOut();
-        });	
+        target = $(this).siblings(code_blocks_with_copy_to_clipboard);
+        copy_element_to_clipboard(target, function(){});
+        $(this).prev().fadeIn().delay(1000).fadeOut()
     });
 
     /* Copy the target element to the clipboard
@@ -85,9 +75,4 @@ $(document).ready(function () {
             temp.remove(); // Remove temporary element.
         }
     }
-
-    $(window).on('resize', function(){
-        $('#copy_to_clipboard').hide();
-        $('#copied_confirmation').hide();
-    });
 });
