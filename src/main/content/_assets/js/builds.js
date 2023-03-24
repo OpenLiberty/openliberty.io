30bc552c96be0c7f3c84fdbecf26f597bfcb02cf
Kin Ueng <github@kinueng.com>
2023-03-24 15:40:14 -0500

    Update src/main/content/_assets/js/builds.js
    This should be case sensitive matching

--- src/main/content/_assets/js/builds.js
+++ src/main/content/_assets/js/builds.js
@@ -218,7 +218,7 @@ function hideOlderVersionsOfDownloadPackages(package_names) {
                 builds[i].package_signature_locations.splice(j_index, 1);
             }
         }
-        if (package_names.includes("webprofile10")) {
+        if (package_names.includes("webProfile10")) {
             var k_index = package_names[i].indexOf("webprofile9");
             if (k_index > -1) {
                 builds[i].package_locations.splice(k_index, 1);
