f29aca76768724e1f44df41c2a70c52345f976de
amyreit <amyreit@us.ibm.com>
2023-06-06 13:24:06 -0500

    Revert "fixing test for ee10 and mp6"

--- src/test/cypressjs/cypress/integration/testRuntimeReleaseVersions.js
+++ src/test/cypressjs/cypress/integration/testRuntimeReleaseVersions.js
@@ -8,35 +8,19 @@ describe('Test that all runtime release versions are in the correct custom order
         let packages = [];
         let listOfPackLists = [[]];
         let currInd = 0;
-
-        // This list has to be updated whenever new levels are added, basically just make sure that
-        // the order represents the top to bottom order in terms of what would be listed first
-        // for example Jakarta EE 8 is never listed higher than Web Profile 9 and MicroProfile 5 but always 
-        // higher than MicroProfile 4 and Web Profile 8
-        
         let orderGuide = [
-            "Jakarta EE 10",
-            "Jakarta EE 9",          
-            "Web Profile 10",
-            "Web Profile 9",           
-            "MicroProfile 6",
+            "Jakarta EE 9",
+            "Web Profile 9",
             "MicroProfile 5",
             "Jakarta EE 8",
-            "Web Profile 8",            
+            "Java EE 8",
+            "Web Profile 8",
             "MicroProfile 4",
+            "MicroProfile 3",
             "Kernel",
             "All GA Features"]
         cy.get("td[headers='runtime_releases_package']").each((elm, i) => {
-            if (elm.text().includes("Jakarta EE 10")) {              
-                packages.push("Jakarta EE 10");
-            } else {
-               if (elm.text().includes("Jakarta EE 8")) {               
-                packages.push("Jakarta EE 8");
-               } else {
-                packages.push(elm.text());
-               }
-            }   
-           
+            packages.push(elm.text());
             if(elm.text() === "All GA Features"){
                 ind.push(i);
             }
@@ -50,7 +34,7 @@ describe('Test that all runtime release versions are in the correct custom order
             }
             listOfPackLists.pop()
         }).then(() => {
-             listOfPackLists.forEach((item) => {
+            listOfPackLists.forEach((item) => {
                 for(let i = 0; i < item.length-1; i++){
                     let itemIndex = orderGuide.indexOf(item[i]);
                     let nextItemIndex = orderGuide.indexOf(item[i+1]);
