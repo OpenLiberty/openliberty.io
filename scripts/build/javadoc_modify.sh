2744148865e100031e428ca5dd4a87cef5312857
Annie Nguyen <aknguyen@us.ibm.com>
2023-06-28 16:17:52 -0500

    add stylesheet for frameless

--- scripts/build/javadoc_modify.sh
+++ scripts/build/javadoc_modify.sh
@@ -11,6 +11,18 @@ modifyStylesheet () {
     mv newstylesheet.css "$1"
 }
 
+modifyStylesheetFrameless () {
+    # Append extra javadoc styling located in the css direcctory to the stylesheet.css located in the javadoc api/spi subdirectories
+    cat src/main/content/antora_ui/src/css/javadoc-extended-stylesheet-frameless.css >> "$1"
+
+    # insert extra import to the beginning of the file
+    sed '1 i\
+    @import url("https://fonts.googleapis.com/css?family=Asap:300,400,500");
+    ' "$1" > newstylesheet.css
+    
+    mv newstylesheet.css "$1"
+}
+
 modifySearch () {
     # add url support to search
     cp src/main/content/_assets/js/javadoc-search.js "$1"
@@ -28,7 +40,10 @@ export -f modifyStylesheet
 export -f modifySearch
 export -f modifyRedirect
 
-find target/jekyll-webapp/docs -name stylesheet.css -exec bash -c 'modifyStylesheet {}' \;
+find target/jekyll-webapp/docs -path "*microprofile*/stylesheet.css" -exec bash -c 'modifyStylesheet {}' \;
+find target/jekyll-webapp/docs -path "*liberty-*/stylesheet.css" -exec bash -c 'modifyStylesheet {}' \;
+find target/jekyll-webapp/docs -path "*io.openliberty*/stylesheet.css" -exec bash -c 'modifyStylesheetFrameless {}' \;
+find target/jekyll-webapp/docs -path "*com.ibm.websphere.appserver*/stylesheet.css" -exec bash -c 'modifyStylesheetFrameless {}' \;
 find target/jekyll-webapp/docs -name search.js  -exec bash -c 'modifySearch {}' \;
 find target/jekyll-webapp/docs -name script.js  -exec bash -c 'modifyRedirect {}' \;
 
