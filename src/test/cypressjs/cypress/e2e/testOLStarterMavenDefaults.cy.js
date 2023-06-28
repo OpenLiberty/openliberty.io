6ba9d50212a4c669a44e032c80d35ea3f835fc05
amyreit <amyreit@us.ibm.com>
2023-06-28 12:38:28 -0500

    Revert "Changing default test"

--- src/test/cypressjs/cypress/e2e/testOLStarterMavenDefaults.cy.js
+++ src/test/cypressjs/cypress/e2e/testOLStarterMavenDefaults.cy.js
@@ -1,6 +1,8 @@
 describe('Test Open Liberty Starter - Maven Default EE and MP', () => {
-    // Test the default values for the starter   
-  
+    // Allow users to specify URL via variables found in cypress.env.json    
+    // java home path for jdk17, jdk11, jdk8 can be change via cypress.env.json base on user env var
+    // this test will only download the starter for the default_jdk in that file
+
     const default_javahome = Cypress.env('jdk_11_home');
     const appname = "app-name-m-default";
     const downloadsFolder = Cypress.config('downloadsFolder');
@@ -19,4 +21,15 @@ describe('Test Open Liberty Starter - Maven Default EE and MP', () => {
         cy.get("#Starter_MicroProfile_Version").should("have.value", mpDefault);
     });
 
+    it('Test zip file is downloaded with default values', () => {
+        cy.exec(`rm -rf ${downloadsFolder}`, { failOnNonZeroExit: false });
+        // download zip for the defaults      
+        cy.log('appname ' + appname);
+        cy.log('javaLevel ' + javaDefault);
+        cy.goToOpenLibertyStarter();
+        cy.downloadAndUnzipFile(appname, eeDefault, mpDefault,'m');
+        cy.runMVNWLibertyDev(appname, default_javahome);
+        cy.checkLocalSplashPage();
+        cy.runMVNWLibertyStop(appname, default_javahome);
+    });
 });
