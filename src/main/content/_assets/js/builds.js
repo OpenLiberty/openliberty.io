68481908214d5c23849d7770dc1e8f74ec57dbbc
Amy Reitmeyer <amyreit@us.ibm.com>
2023-06-29 13:27:40 -0500

    again

--- src/main/content/_assets/js/builds.js
+++ src/main/content/_assets/js/builds.js
@@ -856,7 +856,7 @@ function add_invalid_message(field_id, valid) {
 // Base package name
 function validate_group_name() {
     // Each group name package can contain letters (either lower or uppercase),   
-    // numbers and underscores all separated by periods Eg: com.Acme.my_widget.v2
+    // numbers and underscores all separated by periods Eg: com.Acme.my_widget.v21
     var valid_syntax = /^[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]*)+$/g;
     var value = $('.starter_field[data-starter-field=\'g\'] input').val();
     var valid = value == '' ? false : valid_syntax.test(value);
