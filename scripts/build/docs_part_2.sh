4daaec799e834f3df406ca07f52879f3f58a261c
Steven Zvonek <6392944+steven1046@users.noreply.github.com>
2023-09-13 09:59:34 -0500

    Test moving javadocs into the war image

--- scripts/build/docs_part_2.sh
+++ scripts/build/docs_part_2.sh
@@ -11,13 +11,6 @@ echo "Total execution time for parsing the features toc: '$(date -u --date @$(( 
 echo "Begin building javadoc content"
 $BUILD_SCRIPTS_DIR/javadoc_clone.sh
 
-# Move the javadocs into the web app
-echo "Moving javadocs to the jekyll webapp"
-timer_start=$(date +%s)
-cp -r src/main/content/docs-javadoc/modules target/jekyll-webapp/docs
-timer_end=$(date +%s)
-echo "Total execution time for copying javadoc to webapp: '$(date -u --date @$(( $timer_end - $timer_start )) +%H:%M:%S)'"
-
 # Special handling for javadocs
 $BUILD_SCRIPTS_DIR/javadoc_modify.sh
 
