e117f2ce9927a20bc0a70a5d594ce7a1a338f088
SteveSamJacob19 <Steve.Sam.Jacob1@ibm.com>
2023-11-27 21:10:28 +0530

    Making final changes

--- src/main/content/_assets/js/builds.js
+++ src/main/content/_assets/js/builds.js
@@ -21,7 +21,7 @@ var versArr = [];
 
 var builds_url = '/api/builds/data';
 var starter_domain = 
-    isNotProdSite() ? 'https://starter-staging.rh9j6zz75er.us-east.codeengine.appdomain.cloud' : 'https://start.openliberty.io';
+    isNotProdSite() ? 'https://starter-staging.rh9j6zz75er.us-east.codeengine.appdomain.cloud' : 'https://localhost:9443';
 var starter_info_url = starter_domain + '/api/start/info';
 var starter_submit_url = starter_domain + '/api/start';
 var failed_builds_request = false;
@@ -1118,9 +1118,6 @@ function displayMessage(message,javaMsg) {
  
     $('.' + classNameIs)
     .append(message);
-
-    $('#starter_warnings li:first-child').css("padding-top","20px");
-    $('#starter_warnings li:last-child').css("padding-bottom","10px");
     $('#starter_warnings').append(close_icon);  
 } 
 
