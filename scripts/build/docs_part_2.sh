3aa4907067739069af251bd15e256b7c8dd30284
natalie-bernhard <natalie.bernhard@ibm.com>
2024-03-19 10:33:25 -0500

    remove commented out code

--- scripts/build/docs_part_2.sh
+++ scripts/build/docs_part_2.sh
@@ -2,19 +2,6 @@
 set -e
 export BUILD_SCRIPTS_DIR=$(dirname $0)
 
-# timer_start=$(date +%s)
-# python3 $BUILD_SCRIPTS_DIR/parse_features_toc.py
-# timer_end=$(date +%s)
-# echo "Total execution time for parsing the features toc: '$(date -u --date @$(( $timer_end - $timer_start )) +%H:%M:%S)'"
-
-# save html for translation here before cloning in Javadocs
-# mkdir -p generated-html
-# cp -r target/jekyll-webapp/docs/. generated-html
-# rm -rf generated-html/*/_images
-# rm -rf generated-html/*/*/_images
-# rm -rf generated-html/*/reference/javadoc
-# rm -rf generated-html/*/reference/feature/*/
-
 # try to free up room for docs
 rm -rf target/jekyll-webapp/docs/*/reference/feature/*/
 
