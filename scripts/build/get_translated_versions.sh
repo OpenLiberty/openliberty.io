228a31634feaf9bc6c62c09181a4a07956892086
natalie-bernhard <natalie.bernhard@ibm.com>
2024-06-05 11:48:59 -0500

    relativize github logo and fix latest

--- scripts/build/get_translated_versions.sh
+++ scripts/build/get_translated_versions.sh
@@ -4,7 +4,10 @@ git fetch --all
 git fetch --all --tags --prune
 tags="$(git tag)"
 for t in $tags; do
-    fold=${t%-*:1}
+    fold=${t%-*}
+    if [ "$fold" != "latest" ]; then
+        fold="${fold:1}";
+    fi
     mkdir -p ../target/jekyll-webapp/ja/docs/$fold
     mkdir -p ../target/jekyll-webapp/zh-Hans/docs/$fold
     git checkout -f -q $t
