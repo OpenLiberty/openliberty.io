910a738ddf90d17c8eaf05d1b9c91a32dcae7bdc
navaneethsnair1 <Navaneeth.S.Nair1@ibm.com>
2024-06-06 21:19:08 +0530

    minor edits on test file

--- src/test/cypressjs/cypress/e2e/ol_api.cy.js
+++ src/test/cypressjs/cypress/e2e/ol_api.cy.js
@@ -1,5 +1,5 @@
 describe('Openliberty API Accesibility Tests', () => {
-    it.only('Overview', () => {
+    it('Overview', () => {
       cy.visit('https://openliberty.io/docs/latest/reference/javadoc/api/oauth-2.0.com.ibm.oauth.core.api.attributes.html?path=24.0.0.3/com.ibm.websphere.appserver.api.oauth_1.2-javadoc/index.html');
       cy.getCompliance("./OL_API/ol_api_overview1");
       cy.visit('https://openliberty.io/docs/latest/reference/javadoc/api/servlet-5.0.com.ibm.websphere.servlet.container.html?path=24.0.0.5/io.openliberty.servlet_1.1-javadoc/index.html');
