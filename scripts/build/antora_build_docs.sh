020d5e41e24fa7cbf309417c290c1a4c2eca5ff2
SteveSamJacob19 <steve.sam.jacob1@ibm.com>
2024-11-27 19:17:28 +0530

    Revert "Merge branch 'sample_prod' into demo1"
    This reverts commit 01e6dfe0670c5ca9944bcce8eadb72606fc885c4, reversing
    changes made to 13af28f4077bc55c54d8b11331730bbff9f10903.

--- scripts/build/antora_build_docs.sh
+++ scripts/build/antora_build_docs.sh
@@ -5,7 +5,7 @@ npm ls -g --depth=0
 if [ "$PROD_SITE" = true ]
   then    
     # Enable google analytics in docs
-    antora --fetch --stacktrace --key google-analytics=G-PXYTWCYE18 src/main/content/docs/antora-playbook.yml
+    antora --fetch --stacktrace --key google-analytics=GTM-TKP3KJ7 src/main/content/docs/antora-playbook.yml
   else
     antora --fetch --stacktrace src/main/content/docs/antora-playbook.yml
 fi
