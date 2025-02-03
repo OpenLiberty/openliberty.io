1b2eb013652b804f8c2a980e06273c0c32492634
SteveSamJacob19 <Steve.Sam.Jacob1@ibm.com>
2025-02-03 16:58:27 +0530

    Edited gzip and securityfilter - translation

--- scripts/build/gzip.sh
+++ scripts/build/gzip.sh
@@ -1,9 +1,20 @@
 timer_start=$(date +%s)
 
 pushd target/jekyll-webapp/docs/
-echo "Runing gzip on all of the docs html."
+echo "Runing gzip on all of the English docs html."
 find . \( -name '*.html' -not -name "*index.html" \) -exec gzip "{}" \;
 popd
+
+pushd target/jekyll-webapp/ja/docs/
+echo "Runing gzip on all of the Japanese docs html."
+find . \( -name '*.html' -not -name "*index.html" \) -exec gzip "{}" \;
+popd
+
+pushd target/jekyll-webapp/zh-Hans/docs/
+echo "Runing gzip on all of Simplified Chinese docs html."
+find . \( -name '*.html' -not -name "*index.html" \) -exec gzip "{}" \;
+popd
+
 
 timer_end=$(date +%s)
 echo "Total execution time for running gzip.sh: '$(date -u --date @$(( $timer_end - $timer_start )) +%H:%M:%S)'"
