7506c1e2b4852496f7ebc75fb9838d593c47d54b
navaneethsnair1 <Navaneeth.S.Nair1@ibm.com>
2025-04-28 14:30:25 +0530

    removing old analytics references

--- scripts/build/antora_build_docs.sh
+++ scripts/build/antora_build_docs.sh
@@ -2,7 +2,4 @@
 echo "Using the Antora playbook to generate what content to display for docs"
 echo "npm analysis antora_build" 
 npm ls -g --depth=0
-if [ "$PROD_SITE" = true ]
-  then    
-    antora --fetch --stacktrace src/main/content/docs/antora-playbook.yml
-fi
+antora --fetch --stacktrace src/main/content/docs/antora-playbook.yml
