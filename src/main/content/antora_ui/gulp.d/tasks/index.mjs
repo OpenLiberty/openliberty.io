3d142d06dcc3f506a391541abf8dfa34e54ff1e6
Navaneeth S Nair <142485870+navaneethsnair1@users.noreply.github.com>
2026-03-17 15:31:59 +0530

    Revert "Update to ES Modules"

--- src/main/content/antora_ui/gulp.d/tasks/index.mjs
+++
@@ -1,9 +0,0 @@
-import build from './build.mjs'
-import buildPreviewPages from './build-preview-pages.mjs'
-import pack from './pack.mjs'
-import remove from './remove.mjs'
-import serve from './serve.mjs'
-
-export { build, buildPreviewPages, pack, remove, serve }
-
-
