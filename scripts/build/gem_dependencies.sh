bf2c951fc1546f961dc1fb0d33f1f682925b6547
Navaneeth S Nair <142485870+navaneethsnair1@users.noreply.github.com>
2026-03-20 10:45:21 +0530

    Revert "update versions of jekyll, bundler and ruby"

--- scripts/build/gem_dependencies.sh
+++ scripts/build/gem_dependencies.sh
@@ -1,9 +1,11 @@
 echo "Installing ruby packages..."
 gem install ffi -v 1.16.3
 gem install public_suffix -v 5.1.1
-gem install jekyll -v 4.4.1
+gem install jekyll -v 3.8.6
+gem install jekyll-assets -v 2.4.0
 gem install jekyll-multiple-languages-plugin
-gem install bundler -v 4.0.8
+gem install bundler -v 2.4.22
 gem install faraday -v 2.8.1
 gem install faraday-net_http -v 3.0.2
-gem install jekyll-feed jekyll-asciidoc jekyll-include-cache coderay octokit webrick
+gem install jekyll-feed jekyll-asciidoc jekyll-include-cache coderay octokit
+gem uninstall -i /usr/local/rvm/gems/ruby-2.4.1@global rubygems-bundler
